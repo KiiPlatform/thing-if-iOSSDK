fb441f46de487ddd1811ac9d5175c8f7012a7fd9
Yongping Chen <yongping.chen@kii.com>
2015-08-05 18:31:59 +0900

    Add scripts to playground

--- IoTCloudSDK/IoTCloudSDK.playground/Contents.swift
+++ IoTCloudSDK/IoTCloudSDK.playground/Contents.swift
@@ -1,5 +1,12 @@
 //: Playground - noun: a place where people can play
+import IoTCloudSDK
 
-import UIKit
+let owner = Owner(ownerID: TypedID(type:"user", id:"user-abcd-efgh"), accessToken: "dummy-token")
+print(owner.ownerID.type)
+let schema = Schema(thingType: "SmartLight-Demo",
+    name: "SmartLight-Demo", version: 1)
+let api = IoTCloudAPIBuilder(appID: "myApp", appKey: "myAppKey",
+    baseURL: "https://api.kii.com/", owner: owner).addSchema(schema).build()
 
-var str = "Hello, playground"
+let target = api.onBoard("th.abcd-efgh", thingPassword: "dummyPassword")
+print(target.description)
