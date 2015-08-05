6b19e944f5fbae3d405704131849521abc0f2ffe
Yongping Chen <yongping.chen@kii.com>
2015-08-05 19:05:38 +0900

    Change playground scripts

--- IoTCloudSDK/IoTCloudSDK.playground/Contents.swift
+++ IoTCloudSDK/IoTCloudSDK.playground/Contents.swift
@@ -2,11 +2,10 @@
 import IoTCloudSDK
 
 let owner = Owner(ownerID: TypedID(type:"user", id:"user-abcd-efgh"), accessToken: "dummy-token")
-print(owner.ownerID.type)
 let schema = Schema(thingType: "SmartLight-Demo",
     name: "SmartLight-Demo", version: 1)
 let api = IoTCloudAPIBuilder(appID: "myApp", appKey: "myAppKey",
     baseURL: "https://api.kii.com/", owner: owner).addSchema(schema).build()
 
-let target = api.onBoard("th.abcd-efgh", thingPassword: "dummyPassword")
+let target = try api.onBoard("th.abcd-efgh", thingPassword: "dummyPassword")
 print(target.description)
