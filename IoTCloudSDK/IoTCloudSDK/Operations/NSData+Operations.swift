f4e02583c7f9578ba6edb101047a5cc9af942d77
Riza <riza.alaudin@kii.com>
2015-08-12 19:02:23 +0900

    progress push installation

---
+++ IoTCloudSDK/IoTCloudSDK/Operations/NSData+Operations.swift
@@ -0,0 +1,20 @@
+//
+//  NSData+Operations.swift
+//  IoTCloudSDK
+//
+//  Created by Syah Riza on 8/12/15.
+//  Copyright © 2015 Kii. All rights reserved.
+//
+
+import Foundation
+
+extension NSData {
+    func hexString() -> String {
+        // "Array" of all bytes:
+        let bytes = UnsafeBufferPointer<UInt8>(start: UnsafePointer(self.bytes), count:self.length)
+        // Array of hex strings, one for each byte:
+        let hexBytes = bytes.map{ String(format: "%02hhx", $0) }
+        // Concatenate all hex strings:
+        return "".join(hexBytes)
+    }
+}
