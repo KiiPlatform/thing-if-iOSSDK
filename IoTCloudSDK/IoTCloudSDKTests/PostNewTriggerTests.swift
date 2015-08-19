7c26e7d0c55e913b95efdc2fc0e957b9bea22e7c
Yongping Chen <yongping.chen@kii.com>
2015-08-19 15:36:47 +0900

    Test for different predicate

--- IoTCloudSDK/IoTCloudSDKTests/PostNewTriggerTests.swift
+++ IoTCloudSDK/IoTCloudSDKTests/PostNewTriggerTests.swift
@@ -31,23 +31,61 @@ class PostNewTriggerTests: XCTestCase {
         super.tearDown()
     }
 
-    //TODO: verify the possible predicate
+    struct TestCase {
+        let statement: Statement
+        let expectedStatementDict: Dictionary<String, AnyObject>
+        let triggersWhen: TriggersWhen
+        let expectedTriggersWhenString: String
+    }
 
-    func testPostNewTriggerSuccess() {
-        let expectation = self.expectationWithDescription("postNewTriggerSuccess")
+    func testPostNewTrigger_success() {
+
+        let orClauseStatement = ["type": "or", "clauses": [["type":"eq","field":"color", "value": 0], ["type": "not", "clause": ["type":"eq","field":"power", "value": true]] ]]
+        let andClauseStatement = ["type": "and", "clauses": [["type":"eq","field":"color", "value": 0], ["type": "not", "clause": ["type":"eq","field":"power", "value": true]] ]]
+        let complexExpectedStatements:[Dictionary<String, AnyObject>] = [
+            ["type": "and", "clauses": [["type":"eq","field":"brightness", "value": 50], orClauseStatement]],
+            ["type": "or", "clauses": [["type":"eq","field":"brightness", "value": 50], andClauseStatement]]
+        ]
+
+        let testsCases: [TestCase] = [
+            // simple statement
+            TestCase(statement: Equals(field: "color", value: 0), expectedStatementDict: ["type":"eq","field":"color", "value": 0], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: Equals(field: "power", value: true), expectedStatementDict: ["type":"eq","field":"power", "value": true], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: NotEquals(field: "power", value: true), expectedStatementDict: ["type": "not", "clause": ["type":"eq","field":"power", "value": true]], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: NotGreaterThan(field: "color", upperLimit: 255), expectedStatementDict: ["type": "range", "field": "color", "upperLimit": 255, "upperIncluded": true], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: LessThan(field: "color", upperLimit: 200), expectedStatementDict: ["type": "range", "field": "color", "upperLimit": 200, "upperIncluded": false], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: NotLessThan(field: "color", lowerLimit: 1), expectedStatementDict: ["type": "range", "field": "color", "lowerLimit": 1, "lowerIncluded": true], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: GreaterThan(field: "color", lowerLimit: 1), expectedStatementDict: ["type": "range", "field": "color", "lowerLimit": 1, "lowerIncluded": false], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: And(statements: Equals(field: "color", value: 0), NotEquals(field: "power", value: true)), expectedStatementDict: ["type": "and", "clauses": [["type":"eq","field":"color", "value": 0], ["type": "not", "clause": ["type":"eq","field":"power", "value": true]] ]], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: Or(statements: Equals(field: "color", value: 0), NotEquals(field: "color", value: true)), expectedStatementDict: ["type": "or", "clauses": [["type":"eq","field":"color", "value": 0], ["type": "not", "clause": ["type":"eq","field":"power", "value": true]] ]], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            // complex statements
+            TestCase(statement: And(statements: Equals(field: "brightness", value: 50), Or(statements: Equals(field: "color", value: 0), NotEquals(field: "power", value: true))), expectedStatementDict: complexExpectedStatements[0], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            TestCase(statement: Or(statements: Equals(field: "brightness", value: 50),And(statements: Equals(field: "color", value: 0), NotEquals(field: "power", value: true))), expectedStatementDict: complexExpectedStatements[1], triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE, expectedTriggersWhenString: "CONDITION_FALSE_TO_TRUE"),
+            // test triggersWhen
+            TestCase(statement: Equals(field: "color", value: 0), expectedStatementDict: ["type":"eq","field":"color", "value": 0], triggersWhen: TriggersWhen.CONDITION_CHANGED, expectedTriggersWhenString: "CONDITION_CHANGED"),
+            TestCase(statement: Equals(field: "color", value: 0), expectedStatementDict: ["type":"eq","field":"color", "value": 0], triggersWhen: TriggersWhen.CONDITION_TRUE, expectedTriggersWhenString: "CONDITION_TRUE")
+
+        ]
+        for (index,testCase) in testsCases.enumerate() {
+            postNewTriggerSuccess("testPostNewTrigger_success_\(index)", testcase: testCase)
+        }
+
+    }
+
+    func postNewTriggerSuccess(tag: String, testcase: TestCase) {
+        let expectation = self.expectationWithDescription(tag)
 
         do{
             let expectedTriggerID = "0267251d9d60-1858-5e11-3dc3-00f3f0b5"
             let target = Target(targetType: TypedID(type: "thing", id: expectedTriggerID))
             let actions: [Dictionary<String, AnyObject>] = [["turnPower":["power":true]],["setBrightness":["bribhtness":90]]]
-            let statement = Equals(field: "color", value: 0)
-            let condition = Condition(statement: statement)
-            let predicate = StatePredicate(condition: condition, triggersWhen: TriggersWhen.CONDITION_FALSE_TO_TRUE)
+            let condition = Condition(statement: testcase.statement)
+            let predicate = StatePredicate(condition: condition, triggersWhen: testcase.triggersWhen)
 
             let expectedActions = [["turnPower":["power":true]],["setBrightness":["bribhtness":90]]]
-            let expectedStatement = ["type":"eq","filed":"color", "value": 0]
+            let expectedStatement = testcase.expectedStatementDict
             let expectedEventSource = "states"
-            let expectedTriggerWhen = "CONDITION_FALSE_TO_TRUE"
+            let expectedTriggerWhen = testcase.expectedTriggersWhenString
             let expectedPredicateDict = ["eventSource":expectedEventSource, "triggersWhen":expectedTriggerWhen, "condition":expectedStatement]
 
             // mock response
@@ -61,7 +99,7 @@ class PostNewTriggerTests: XCTestCase {
                 //verify header
                 let expectedHeader = ["authorization": "Bearer \(self.owner.accessToken)", "Content-type":"application/json"]
                 for (key, value) in expectedHeader {
-                    XCTAssertEqual(value, request.valueForHTTPHeaderField(key))
+                    XCTAssertEqual(value, request.valueForHTTPHeaderField(key), tag)
                 }
                 //verify body
 
@@ -69,9 +107,9 @@ class PostNewTriggerTests: XCTestCase {
                 do {
                     let expectedBodyData = try NSJSONSerialization.dataWithJSONObject(expectedBody, options: NSJSONWritingOptions(rawValue: 0))
                     let actualBodyData = request.HTTPBody
-                    XCTAssertTrue(expectedBodyData.length == actualBodyData!.length)
+                    XCTAssertTrue(expectedBodyData.length == actualBodyData!.length, tag)
                 }catch(_){
-                    XCTFail()
+                    XCTFail(tag)
                 }
             }
             MockSession.mockResponse = (jsonData, urlResponse: urlResponse, error: nil)
@@ -80,13 +118,13 @@ class PostNewTriggerTests: XCTestCase {
 
             api.postNewTrigger(target, schemaName: schema.name, schemaVersion: schema.version, actions: actions, predicate: predicate, completionHandler: { (trigger, error) -> Void in
                 if error == nil{
-                    XCTAssertEqual(trigger!.triggerID, expectedTriggerID)
-                    XCTAssertEqual(trigger!.targetID.toString(), target.targetType.toString())
-                    XCTAssertEqual(trigger!.enabled, true)
-                    XCTAssertNotNil(trigger!.predicate)
-                    XCTAssertEqual(trigger!.command.commandID, "")
+                    XCTAssertEqual(trigger!.triggerID, expectedTriggerID, tag)
+                    XCTAssertEqual(trigger!.targetID.toString(), target.targetType.toString(), tag)
+                    XCTAssertEqual(trigger!.enabled, true, tag)
+                    XCTAssertNotNil(trigger!.predicate, tag)
+                    XCTAssertEqual(trigger!.command.commandID, "", tag)
                 }else {
-                    XCTFail("should success")
+                    XCTFail("should success for \(tag)")
                 }
                 expectation.fulfill()
             })
@@ -95,7 +133,7 @@ class PostNewTriggerTests: XCTestCase {
         }
         self.waitForExpectationsWithTimeout(20.0) { (error) -> Void in
             if error != nil {
-                XCTFail("execution timeout")
+                XCTFail("execution timeout for \(tag)")
             }
         }
     }
