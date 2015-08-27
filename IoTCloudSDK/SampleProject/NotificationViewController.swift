c3cecc0fc359041afba261d5baac27ac6f873bab
Riza <riza.alaudin@kii.com>
2015-08-27 15:16:49 +0900

    progress

---
+++ IoTCloudSDK/SampleProject/NotificationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.swift
+//  SampleProject
+//
+//  Created by Syah Riza on 8/27/15.
+//  Copyright © 2015 Kii Corporation. All rights reserved.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+}
