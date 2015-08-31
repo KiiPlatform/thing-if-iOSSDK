1b909b5d9ff9bbf58532747ebba8a0e057d1f7c5
Yongping Chen <yongping.chen@kii.com>
2015-08-31 19:31:01 +0900

    Implement python script to generate swift docs

---
+++ IoTCloudSDK/Documentation/jazzy-options.js
@@ -0,0 +1,35 @@
+{
+    "project": "IoTCloudSDK.xcodeproj",
+    "scheme": "IoTCloudSDK",
+
+    "author": "Kii Corp",
+    "author_url": "http://kii.com",
+    "github_url": "https://github.com/KiiPlatform/IoTCloud-iOSSDK",
+    "output": "./Documentation/docs/",
+    "swift-version": "2",
+
+    "ignore": [
+      "BlockObserver.swift",
+      "NSData+Operations.swift",
+      "OperationErrors.swift",
+      "BlockOperation.swift",
+      "NSLock+Operations.swift",
+      "OperationObserver.swift",
+      "DelayOperation.swift",
+      "NSOperation+Operations.swift",
+      "OperationQueue.swift",
+      "Dictionary+Operations.swift",
+      "NegatedCondition.swift",
+      "ReachabilityCondition.swift",
+      "ExclusivityController.swift",
+      "NoCancelledDependencies.swift",
+      "TimeoutObserver.swift",
+      "GroupOperation.swift",
+      "Operation.swift",
+      "URLSessionTaskOperation.swift",
+      "MutuallyExclusive.swift",
+      "OperationCondition.swift",
+      "IoTRequestOperation.swift",
+      "NSNumber+checkTypes.swift"
+    ]
+}
