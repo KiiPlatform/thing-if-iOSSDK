84d5f66d556d77f273b91d64f7531b3bc8ee8a5f
Riza <riza.alaudin@kii.com>
2015-10-06 14:26:46 +0900

    Add accessToken on Target (Fix spreadsheet no 560)

--- IoTCloudSDK/IoTCloudSDK/Target.swift
+++ IoTCloudSDK/IoTCloudSDK/Target.swift
@@ -10,20 +10,26 @@ public class Target : NSObject, NSCoding {
     // MARK: - Implements NSCoding protocol
     public func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(self.targetType, forKey: "targetType")
+        aCoder.encodeObject(self.accessToken, forKey: "accessToken")
     }
 
     // MARK: - Implements NSCoding protocol
     public required init(coder aDecoder: NSCoder) {
         self.targetType = aDecoder.decodeObjectForKey("targetType") as! TypedID
-
+        self.accessToken = aDecoder.decodeObjectForKey("accessToken") as! String?
     }
+
+    /** Access token of the target. */
+    public let accessToken: String?
 
     /** Init with TypedID
 
     - Parameter targetType: ID of target
+    - Parameter accessToken: Access token of the target, can nil.
     */
-    public init(targetType: TypedID) {
+    public init(targetType: TypedID, accessToken : String? = nil) {
         self.targetType = targetType
+        self.accessToken = accessToken
     }
 
     public override func isEqual(object: AnyObject?) -> Bool {
@@ -31,6 +37,6 @@ public class Target : NSObject, NSCoding {
             return false
         }
 
-        return self.targetType == aTarget.targetType
+        return self.targetType == aTarget.targetType && self.accessToken == aTarget.accessToken
     }
 }
