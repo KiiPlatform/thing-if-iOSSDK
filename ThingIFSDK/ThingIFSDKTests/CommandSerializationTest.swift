d6311c0a3eece3595338d0c80e42ab525445a51c
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-03-01 16:55:48 +0900

    CommandSerializationTest.swift is added.

---
+++ ThingIFSDK/ThingIFSDKTests/CommandSerializationTest.swift
@@ -0,0 +1,20 @@
+//
+//  CommandSerializationTest.swift
+//  ThingIFSDK
+//
+//  Created on 2016/03/01.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+import XCTest
+
+class CommandSerializationTest: SmallTestBase {
+
+    override func setUp() {
+        super.setUp()
+    }
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
