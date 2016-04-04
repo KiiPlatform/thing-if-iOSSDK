b2cce1138fa981b0af837ce9f879e65338ba6825
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-04-04 13:53:03 +0900

    CommandForm.swift is added.

---
+++ ThingIFSDK/ThingIFSDK/CommandForm.swift
@@ -0,0 +1,12 @@
+//
+//  CommandForm.swift
+//  ThingIFSDK
+//
+//  Created on 2016/04/04.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+import Foundation
+
+public class CommandForm: NSObject {
+}
