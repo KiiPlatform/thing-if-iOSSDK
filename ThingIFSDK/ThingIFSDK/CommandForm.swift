5fad81546f802cfa889ad914e4e49f6232cc7f4b
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-04-04 15:43:43 +0900

    CommandForm is designed.

--- ThingIFSDK/ThingIFSDK/CommandForm.swift
+++ ThingIFSDK/ThingIFSDK/CommandForm.swift
@@ -9,4 +9,50 @@
 import Foundation
 
 public class CommandForm: NSObject {
+
+    internal let schemaName: String
+    internal let schemaVersion: Int
+    internal let actions: [Dictionary<String, AnyObject>]
+
+    public var title: String? {
+        get {
+            return self.title
+        }
+
+        set (title){
+            // TODO: implement me.
+            self.title = title
+        }
+    }
+
+    public var desc: String? {
+        get {
+            return self.desc
+        }
+
+        set (desc) {
+            self.desc = desc
+        }
+    }
+
+    public var metadata: [Dictionary<String, AnyObject>]? {
+        get {
+            return self.metadata
+        }
+
+        set (metadata) {
+            self.metadata = metadata
+        }
+    }
+
+    public init(schemaName: String,
+                schemaVersion: Int,
+                actions: [Dictionary<String, AnyObject>])
+    {
+        // TODO: implement me.
+        self.schemaName = schemaName
+        self.schemaVersion = schemaVersion
+        self.actions = actions
+    }
+
 }
