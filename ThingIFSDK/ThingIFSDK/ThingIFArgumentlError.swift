76906f1289abd7762bbc11e21e3694e77f4d03ee
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-04-06 16:36:55 +0900

    ThingIFArgumentlError is added.

---
+++ ThingIFSDK/ThingIFSDK/ThingIFArgumentlError.swift
@@ -0,0 +1,22 @@
+//
+//  ThingIFArgumentError.swift
+//  ThingIFSDK
+//
+//  Created on 2016/04/06.
+//  Copyright (c) 2016 Kii. All rights reserved.
+//
+
+import Foundation
+import Swift
+
+/**
+Argument errors for Thing-IF.
+*/
+enum ThingIFArgumentError: ErrorType {
+
+    /**
+    If a argument of a method is invalid, this error is thrown.
+    */
+    case InvalidArgument(message: String)
+
+}
