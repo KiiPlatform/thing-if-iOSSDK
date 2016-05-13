fa0b2a841044809c02608ba0cb8f8002d65c3f84
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2016-05-13 16:13:07 +0900

    remove NSCoding from GatewayInformation.

--- ThingIFSDK/ThingIFSDK/Gateway/GatewayInformation.swift
+++ ThingIFSDK/ThingIFSDK/Gateway/GatewayInformation.swift
@@ -7,22 +7,11 @@
 
 import Foundation
 
-public class GatewayInformation: NSObject, NSCoding {
+public class GatewayInformation {
 
     public let vendorThingID: String
 
-    // MARK: - Implements NSCoding protocol
-    public func encodeWithCoder(aCoder: NSCoder)
-    {
-        aCoder.encodeObject(self.vendorThingID, forKey: "vendorThingID")
-    }
-
-    public required init(coder aDecoder: NSCoder)
-    {
-        self.vendorThingID = aDecoder.decodeObjectForKey("vendorThingID") as! String
-    }
-
-    public init(vendorThingID: String)
+    init(vendorThingID: String)
     {
         self.vendorThingID = vendorThingID
     }
