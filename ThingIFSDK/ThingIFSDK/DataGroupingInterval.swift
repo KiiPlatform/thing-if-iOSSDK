17dbe03c46901aa1720d87ea3ec3637033256e49
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2016-07-20 19:10:46 +0900

    change enum.

--- ThingIFSDK/ThingIFSDK/DataGroupingInterval.swift
+++ ThingIFSDK/ThingIFSDK/DataGroupingInterval.swift
@@ -7,25 +7,10 @@
 
 import Foundation
 
-public enum DataGroupingInterval {
-    case INTERVAL_1_MINUTE
-    case INTERVAL_15_MINUTES
-    case INTERVAL_30_MINUTES
-    case INTERVAL_1_HOUR
-    case INTERVAL_12_HOURS
-
-    public func getInterval() -> String {
-        switch self {
-        case .INTERVAL_1_MINUTE:
-            return "1_MINUTE"
-        case .INTERVAL_15_MINUTES:
-            return "15_MINUTES"
-        case .INTERVAL_30_MINUTES:
-            return "30_MINUTES"
-        case .INTERVAL_1_HOUR:
-            return "1_HOUR"
-        case .INTERVAL_12_HOURS:
-            return "12_HOURS"
-        }
-    }
+public enum DataGroupingInterval: String {
+    case INTERVAL_1_MINUTE = "1_MINUTE"
+    case INTERVAL_15_MINUTES = "15_MINUTES"
+    case INTERVAL_30_MINUTES = "30_MINUTES"
+    case INTERVAL_1_HOUR = "1_HOUR"
+    case INTERVAL_12_HOURS = "12_HOURS"
 }
