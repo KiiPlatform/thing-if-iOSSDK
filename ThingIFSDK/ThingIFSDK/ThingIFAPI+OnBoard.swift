82ddf16451efa391ab67301ca803350dfcaaba6b
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2016-08-02 16:17:54 +0900

    create inner method.

--- ThingIFSDK/ThingIFSDK/ThingIFAPI+OnBoard.swift
+++ ThingIFSDK/ThingIFSDK/ThingIFAPI+OnBoard.swift
@@ -96,4 +96,85 @@ extension ThingIFAPI {
             }
     }
 
+    func _onboardEndnodeWithGateway(
+        pendingEndnode:PendingEndNode,
+        endnodePassword:String,
+        dataGroupingInterval:DataGroupingInterval? = nil,
+        completionHandler: (EndNode?, ThingIFError?)-> Void
+        ) ->Void
+    {
+        if self.target == nil || !(self.target is Gateway) {
+            completionHandler(nil, ThingIFError.TARGET_NOT_AVAILABLE)
+            return
+        }
+        if pendingEndnode.vendorThingID == nil || pendingEndnode.vendorThingID!.isEmpty {
+            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
+            return
+        }
+        if endnodePassword.isEmpty {
+            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
+            return
+        }
+
+        let requestURL = "\(self.baseURL)/thing-if/apps/\(self.appID)/onboardings"
+
+        // generate header
+        let requestHeaderDict:Dictionary<String, String> = [
+            "x-kii-appid": self.appID,
+            "x-kii-appkey": self.appKey,
+            "authorization": "Bearer \(self.owner.accessToken)",
+            "Content-Type": "application/vnd.kii.OnboardingEndNodeWithGatewayThingID+json"
+        ]
+
+        // genrate body
+        let requestBodyDict = NSMutableDictionary(dictionary:
+            [
+                "gatewayThingID": self.target!.typedID.id,
+                "endNodeVendorThingID": pendingEndnode.vendorThingID!,
+                "endNodePassword": endnodePassword,
+                "owner": self.owner.typedID.toString()
+            ]
+        )
+
+        if pendingEndnode.thingType != nil {
+            requestBodyDict["endNodeThingType"] = pendingEndnode.thingType
+        }
+
+        if !(pendingEndnode.thingProperties?.isEmpty ?? true) {
+            requestBodyDict["endNodeThingProperties"] = pendingEndnode.thingProperties
+        }
+
+        if dataGroupingInterval != nil {
+            requestBodyDict["dataGroupingInterval"] = dataGroupingInterval!.rawValue
+        }
+
+        do {
+            let requestBodyData = try NSJSONSerialization.dataWithJSONObject(requestBodyDict, options: NSJSONWritingOptions(rawValue: 0))
+            // do request
+            let request = buildDefaultRequest(
+                HTTPMethod.POST,
+                urlString: requestURL,
+                requestHeaderDict: requestHeaderDict,
+                requestBodyData: requestBodyData,
+                completionHandler: { (response, error) -> Void in
+                    let endNode: EndNode?
+                    if error != nil {
+                        endNode = nil
+                    } else {
+                        let thingID = response?["endNodeThingID"] as! String
+                        let accessToken = response?["accessToken"] as! String
+                        endNode = EndNode(thingID: thingID, vendorThingID: pendingEndnode.vendorThingID!, accessToken: accessToken)
+                    }
+                    dispatch_async(dispatch_get_main_queue()) {
+                        completionHandler(endNode, error)
+                    }
+                }
+            )
+            let operation = IoTRequestOperation(request: request)
+            operationQueue.addOperation(operation)
+        } catch(_) {
+            kiiSevereLog("ThingIFError.JSON_PARSE_ERROR")
+            completionHandler(nil, ThingIFError.JSON_PARSE_ERROR)
+        }
+    }
 }
--- ThingIFSDK/ThingIFSDK/ThingIFAPI.swift
+++ ThingIFSDK/ThingIFSDK/ThingIFAPI.swift
@@ -227,75 +227,9 @@ public class ThingIFAPI: NSObject, NSCoding {
         completionHandler: (EndNode?, ThingIFError?)-> Void
         ) ->Void
     {
-        if (self.target == nil) || !(self.target is Gateway) {
-            completionHandler(nil, ThingIFError.TARGET_NOT_AVAILABLE)
-            return
-        }
-        if pendingEndnode.vendorThingID == nil || pendingEndnode.vendorThingID!.isEmpty {
-            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
-            return
-        }
-        if endnodePassword.isEmpty {
-            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
-            return
-        }
-
-        let requestURL = "\(self.baseURL)/thing-if/apps/\(self.appID)/onboardings"
-
-        // generate header
-        let requestHeaderDict:Dictionary<String, String> = [
-            "x-kii-appid": self.appID,
-            "x-kii-appkey": self.appKey,
-            "authorization": "Bearer \(self.owner.accessToken)",
-            "Content-Type": "application/vnd.kii.OnboardingEndNodeWithGatewayThingID+json"
-        ]
-
-        // genrate body
-        let requestBodyDict = NSMutableDictionary(dictionary:
-            [
-                "gatewayThingID": self.target!.typedID.id,
-                "endNodeVendorThingID": pendingEndnode.vendorThingID!,
-                "endNodePassword": endnodePassword,
-                "owner": self.owner.typedID.toString()
-            ]
-        )
-
-        if pendingEndnode.thingType != nil {
-            requestBodyDict["endNodeThingType"] = pendingEndnode.thingType
-        }
-
-        if !(pendingEndnode.thingProperties?.isEmpty ?? true) {
-            requestBodyDict["endNodeThingProperties"] = pendingEndnode.thingProperties
-        }
-
-        do {
-            let requestBodyData = try NSJSONSerialization.dataWithJSONObject(requestBodyDict, options: NSJSONWritingOptions(rawValue: 0))
-            // do request
-            let request = buildDefaultRequest(
-                HTTPMethod.POST,
-                urlString: requestURL,
-                requestHeaderDict: requestHeaderDict,
-                requestBodyData: requestBodyData,
-                completionHandler: { (response, error) -> Void in
-                    let endNode: EndNode?
-                    if error != nil {
-                        endNode = nil
-                    } else {
-                        let thingID = response?["endNodeThingID"] as! String
-                        let accessToken = response?["accessToken"] as! String
-                        endNode = EndNode(thingID: thingID, vendorThingID: pendingEndnode.vendorThingID!, accessToken: accessToken)
-                    }
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completionHandler(endNode, error)
-                    }
-                }
-            )
-            let operation = IoTRequestOperation(request: request)
-            operationQueue.addOperation(operation)
-        } catch(_) {
-            kiiSevereLog("ThingIFError.JSON_PARSE_ERROR")
-            completionHandler(nil, ThingIFError.JSON_PARSE_ERROR)
-        }
+        _onboardEndnodeWithGateway(pendingEndnode,
+            endnodePassword: endnodePassword,
+            completionHandler: completionHandler)
     }
 
     /** Endpoints execute onboarding for the thing and merge MQTT channel to the gateway.
@@ -313,81 +247,10 @@ public class ThingIFAPI: NSObject, NSCoding {
         completionHandler: (EndNode?, ThingIFError?)-> Void
         ) ->Void
     {
-        if (self.target == nil) || !(self.target is Gateway) {
-            completionHandler(nil, ThingIFError.TARGET_NOT_AVAILABLE)
-            return
-        }
-        if pendingEndnode.vendorThingID == nil || pendingEndnode.vendorThingID!.isEmpty {
-            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
-            return
-        }
-        if endnodePassword.isEmpty {
-            completionHandler(nil, ThingIFError.UNSUPPORTED_ERROR)
-            return
-        }
-
-        let requestURL = "\(self.baseURL)/thing-if/apps/\(self.appID)/onboardings"
-
-        // generate header
-        let requestHeaderDict:Dictionary<String, String> = [
-            "x-kii-appid": self.appID,
-            "x-kii-appkey": self.appKey,
-            "authorization": "Bearer \(self.owner.accessToken)",
-            "Content-Type": "application/vnd.kii.OnboardingEndNodeWithGatewayThingID+json"
-        ]
-
-        // genrate body
-        let requestBodyDict = NSMutableDictionary(dictionary:
-            [
-                "gatewayThingID": self.target!.typedID.id,
-                "endNodeVendorThingID": pendingEndnode.vendorThingID!,
-                "endNodePassword": endnodePassword,
-                "owner": self.owner.typedID.toString()
-            ]
-        )
-
-        if pendingEndnode.thingType != nil {
-            requestBodyDict["endNodeThingType"] = pendingEndnode.thingType
-        }
-
-        if !(pendingEndnode.thingProperties?.isEmpty ?? true) {
-            requestBodyDict["endNodeThingProperties"] = pendingEndnode.thingProperties
-        }
-
-        if options != nil {
-            if options!.dataGroupingInterval != nil {
-                requestBodyDict["dataGroupingInterval"] = options!.dataGroupingInterval!.rawValue
-            }
-        }
-
-        do {
-            let requestBodyData = try NSJSONSerialization.dataWithJSONObject(requestBodyDict, options: NSJSONWritingOptions(rawValue: 0))
-            // do request
-            let request = buildDefaultRequest(
-                HTTPMethod.POST,
-                urlString: requestURL,
-                requestHeaderDict: requestHeaderDict,
-                requestBodyData: requestBodyData,
-                completionHandler: { (response, error) -> Void in
-                    let endNode: EndNode?
-                    if error != nil {
-                        endNode = nil
-                    } else {
-                        let thingID = response?["endNodeThingID"] as! String
-                        let accessToken = response?["accessToken"] as! String
-                        endNode = EndNode(thingID: thingID, vendorThingID: pendingEndnode.vendorThingID!, accessToken: accessToken)
-                    }
-                    dispatch_async(dispatch_get_main_queue()) {
-                        completionHandler(endNode, error)
-                    }
-                }
-            )
-            let operation = IoTRequestOperation(request: request)
-            operationQueue.addOperation(operation)
-        } catch(_) {
-            kiiSevereLog("ThingIFError.JSON_PARSE_ERROR")
-            completionHandler(nil, ThingIFError.JSON_PARSE_ERROR)
-        }
+        _onboardEndnodeWithGateway(pendingEndnode,
+            endnodePassword: endnodePassword,
+            dataGroupingInterval: options?.dataGroupingInterval,
+            completionHandler: completionHandler)
     }
 
     // MARK: - Push notification methods
