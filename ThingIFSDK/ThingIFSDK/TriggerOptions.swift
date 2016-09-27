2d071613de6abfbcc1a3a975cdcf84335d596b03
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-09-27 13:12:57 +0900

    TriggerOptions is designed.

--- ThingIFSDK/ThingIFSDK/TriggerOptions.swift
+++ ThingIFSDK/ThingIFSDK/TriggerOptions.swift
@@ -25,7 +25,6 @@ public class TriggerOptions: NSObject, NSCoding {
         self.metadata = metadata
     }
 
-
     public func encodeWithCoder(aCoder: NSCoder) {
         aCoder.encodeObject(self.title, forKey: "title")
         aCoder.encodeObject(self.triggerDescription,
@@ -44,11 +43,66 @@ public class TriggerOptions: NSObject, NSCoding {
 
 public class TriggerOptionsBuilder: NSObject, NSCoding {
 
-    public func encodeWithCoder(aCoder: NSCoder) {
+    private var title: String?
+
+    private var triggerDescription: String?
+
+    private var metadata: Dictionary<String, AnyObject>?
+
+    public init(title: String?,
+                 triggerDescription: String?,
+                 metadata: Dictionary<String, AnyObject>?)
+    {
+        self.title = title
+        self.triggerDescription = triggerDescription
+        self.metadata = metadata
+    }
+
+    public func getTitle() -> String? {
+        return self.title;
+    }
+
+    public func setTitle(title: String?) -> TriggerOptionsBuilder {
         // TODO: implement me.
+        return self;
+    }
+
+    public func getTriggerDescription() -> String? {
+        return self.triggerDescription;
+    }
+
+    public func setTriggerDescription(
+            triggerDescription: String?)
+        -> TriggerOptionsBuilder
+    {
+        // TODO: implement me.
+        return self;
+    }
+
+    public func getMetadata() -> Dictionary<String, AnyObject>? {
+        return self.metadata;
+    }
+
+    public func setMetadata(
+            metadata: Dictionary<String, AnyObject>?)
+        -> TriggerOptionsBuilder
+    {
+        // TODO: implement me.
+        return self;
+    }
+
+    public func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(self.title, forKey: "title")
+        aCoder.encodeObject(self.triggerDescription,
+                forKey: "triggerDescription")
+        aCoder.encodeObject(self.metadata, forKey: "metadata")
     }
 
     public required init?(coder aDecoder: NSCoder) {
-        // TODO: implement me.
+        self.title = aDecoder.decodeObjectForKey("title") as? String
+        self.triggerDescription =
+            aDecoder.decodeObjectForKey("triggerDescription") as? String
+        self.metadata = aDecoder.decodeObjectForKey("metadata")
+                as? Dictionary<String, AnyObject>
     }
 }
