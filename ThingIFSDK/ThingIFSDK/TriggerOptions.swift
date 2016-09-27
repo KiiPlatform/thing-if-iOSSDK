561c8427f061512079b9fd733ec7395a1cc0157f
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-09-27 11:23:00 +0900

    TriggerOptions is designed.

--- ThingIFSDK/ThingIFSDK/TriggerOptions.swift
+++ ThingIFSDK/ThingIFSDK/TriggerOptions.swift
@@ -10,12 +10,35 @@ import Foundation
 
 public class TriggerOptions: NSObject, NSCoding {
 
+    public let title: String?
+
+    public let triggerDescription: String?
+
+    public let metadata: Dictionary<String, AnyObject>?
+
+    private init(title: String?,
+                 triggerDescription: String?,
+                 metadata: Dictionary<String, AnyObject>?)
+    {
+        self.title = title
+        self.triggerDescription = triggerDescription
+        self.metadata = metadata
+    }
+
+
     public func encodeWithCoder(aCoder: NSCoder) {
-        // TODO: implement me.
+        aCoder.encodeObject(self.title, forKey: "title")
+        aCoder.encodeObject(self.triggerDescription,
+                forKey: "triggerDescription")
+        aCoder.encodeObject(self.metadata, forKey: "metadata")
     }
 
     public required init?(coder aDecoder: NSCoder) {
-        // TODO: implement me.
+        self.title = aDecoder.decodeObjectForKey("title") as? String
+        self.triggerDescription =
+            aDecoder.decodeObjectForKey("triggerDescription") as? String
+        self.metadata = aDecoder.decodeObjectForKey("metadata")
+                as? Dictionary<String, AnyObject>
     }
 }
 
