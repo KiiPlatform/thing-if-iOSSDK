f618c5b32dcd0f1d23c8ba7e1c6dc56a9cdadb89
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-09-27 15:15:23 +0900

    TriggerOptions is documented.

--- ThingIFSDK/ThingIFSDK/TriggerOptions.swift
+++ ThingIFSDK/ThingIFSDK/TriggerOptions.swift
@@ -8,12 +8,24 @@
 
 import Foundation
 
+/**
+Options of trigger.
+
+This class contains optional data in order to create and modify
+`Trigger` with following methods:
+
+ - `ThingIFAPI.postNewTrigger(form:predicate:options:completionHandler:)`
+ - `ThingIFAPI.patchTrigger(triggerID:form:predicate:options:completionHandler:)`
+*/
 public class TriggerOptions: NSObject, NSCoding {
 
+    /// Title of a command.
     public let title: String?
 
+    /// Description of a trigger.
     public let triggerDescription: String?
 
+    /// Meta data of a trigger.
     public let metadata: Dictionary<String, AnyObject>?
 
     private init(title: String?,
@@ -41,6 +53,9 @@ public class TriggerOptions: NSObject, NSCoding {
     }
 }
 
+/**
+Builder of `TriggerOptions`.
+*/
 public class TriggerOptionsBuilder: NSObject, NSCoding {
 
     private var title: String?
@@ -58,19 +73,50 @@ public class TriggerOptionsBuilder: NSObject, NSCoding {
         self.metadata = metadata
     }
 
+    /**
+    Build `TriggerOptions` instance.
+
+    - returns: `TriggerOptions` instance
+    */
+    public func build() -> TriggerOptions {
+        return TriggerOptions(
+                title: self.title,
+                triggerDescription: self.triggerDescription,
+                metadata: self.metadata)
+    }
+
+    /** Getter of title of this trigger.
+
+    - returns: title of this trigger.
+    */
     public func getTitle() -> String? {
         return self.title;
     }
 
+    /** Setter of title of this trigger.
+
+    - Parameter title: title, This should be equal or less than 50 characters.
+    - returns: this instance to chain.
+    */
     public func setTitle(title: String?) -> TriggerOptionsBuilder {
         // TODO: implement me.
         return self;
     }
 
+    /** Getter of description of this trigger.
+
+    - returns: description of this trigger.
+    */
     public func getTriggerDescription() -> String? {
         return self.triggerDescription;
     }
 
+    /** Setter of description of this trigger.
+
+    - Parameter description: description, This should be equal or less
+      than 200 characters.
+    - returns: this instance to chain.
+    */
     public func setTriggerDescription(
             triggerDescription: String?)
         -> TriggerOptionsBuilder
@@ -79,10 +125,19 @@ public class TriggerOptionsBuilder: NSObject, NSCoding {
         return self;
     }
 
+    /** Getter of metadata of this trigger.
+
+    - returns: metadata of this trigger.
+    */
     public func getMetadata() -> Dictionary<String, AnyObject>? {
         return self.metadata;
     }
 
+    /** Setter of metadata of this trigger.
+
+    - Parameter metadata: metadata
+    - returns: this instance to chain.
+    */
     public func setMetadata(
             metadata: Dictionary<String, AnyObject>?)
         -> TriggerOptionsBuilder
