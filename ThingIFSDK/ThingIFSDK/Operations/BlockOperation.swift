9f52e2b0818aba374be83d514485c18dc06bc441
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-10-26 17:19:32 +0900

    escaping annotation is added to OperationBlock.

--- ThingIFSDK/ThingIFSDK/Operations/BlockOperation.swift
+++ ThingIFSDK/ThingIFSDK/Operations/BlockOperation.swift
@@ -9,7 +9,7 @@ This code shows how to create a simple subclass of Operation.
 import Foundation
 
 /// A closure type that takes a closure as its parameter.
-typealias OperationBlock = ((Void) -> Void) -> Void
+typealias OperationBlock = (@escaping (Void) -> Void) -> Void
 
 /// A sublcass of `Operation` to execute a closure.
 class BlockOperation: Operation {
