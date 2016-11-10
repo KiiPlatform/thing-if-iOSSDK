KiiPlatform/IoTCloud-iOSSDK

add info to error.

ThingIFSDK/ThingIFSDK/Gateway/GatewayAPI.swift
@@ -514,9 +514,9 @@ open class GatewayAPI: NSObject, NSCoding {
         if let dict = UserDefaults.standard.object(forKey: baseKey) as? NSDictionary {
             if dict.object(forKey: key) != nil {
 
-                let sdkVersion = dict.object(forKey: versionKey) as? String
-                if isLoadable(sdkVersion) == false {
-                    throw ThingIFError.api_UNLOADABLE
+                let storedSDKVersion = dict.object(forKey: versionKey) as? String
+                if isLoadable(storedSDKVersion) == false {
+                    throw ThingIFError.api_UNLOADABLE(tag: tag, storedVersion: storedSDKVersion, minimumVersion: MINIMUM_LOADABLE_SDK_VERSION)
                 }
 
                 if let data = dict[key] as? Data {
@@ -529,10 +529,10 @@ open class GatewayAPI: NSObject, NSCoding {
                     throw ThingIFError.invalid_STORED_API
                 }
             } else {
-                throw ThingIFError.api_NOT_STORED
+                throw ThingIFError.api_NOT_STORED(tag: tag)
             }
         } else {
-            throw ThingIFError.api_NOT_STORED
+            throw ThingIFError.api_NOT_STORED(tag: tag)
         }
     }
 

ThingIFSDK/ThingIFSDK/ThingIFAPI.swift
@@ -10,11 +10,11 @@ open class ThingIFAPI: NSObject, NSCoding {
 
     fileprivate static let SHARED_NSUSERDEFAULT_KEY_INSTANCE = "ThingIFAPI_INSTANCE"
     fileprivate static func getStoredInstanceKey(_ tag : String?) -> String{
-        return SHARED_NSUSERDEFAULT_KEY_INSTANCE + (tag == nil ? "" : "_\(tag)")
+        return SHARED_NSUSERDEFAULT_KEY_INSTANCE + (tag == nil ? "" : "_\(tag!)")
     }
     fileprivate static let SHARED_NSUSERDEFAULT_SDK_VERSION_KEY = "ThingIFAPI_VERSION"
     fileprivate static func getStoredSDKVersionKey(_ tag : String?) -> String{
-        return SHARED_NSUSERDEFAULT_SDK_VERSION_KEY + (tag == nil ? "" : "_\(tag)")
+        return SHARED_NSUSERDEFAULT_SDK_VERSION_KEY + (tag == nil ? "" : "_\(tag!)")
     }
     fileprivate static let MINIMUM_LOADABLE_SDK_VERSION = "0.13.0"
 
@@ -812,9 +812,9 @@ open class ThingIFAPI: NSObject, NSCoding {
         {
             if dict.object(forKey: key) != nil {
 
-                let sdkVersion = dict.object(forKey: versionKey) as? String
-                if isLoadable(sdkVersion) == false {
-                    throw ThingIFError.api_UNLOADABLE
+                let storedSDKVersion = dict.object(forKey: versionKey) as? String
+                if isLoadable(storedSDKVersion) == false {
+                    throw ThingIFError.api_UNLOADABLE(tag: tag, storedVersion: storedSDKVersion, minimumVersion: MINIMUM_LOADABLE_SDK_VERSION)
                 }
 
                 if let data = dict[key] as? Data {
@@ -827,10 +827,10 @@ open class ThingIFAPI: NSObject, NSCoding {
                     throw ThingIFError.invalid_STORED_API
                 }
             } else {
-                throw ThingIFError.api_NOT_STORED
+                throw ThingIFError.api_NOT_STORED(tag: tag)
             }
         }else{
-            throw ThingIFError.api_NOT_STORED
+            throw ThingIFError.api_NOT_STORED(tag: tag)
         }
     }
     /** Save this instance

ThingIFSDK/ThingIFSDK/ThingIFError.swift
@@ -29,9 +29,9 @@ public enum ThingIFError : Error {
     /** where target not found */
     case target_NOT_AVAILABLE
     /** when trying to load API from persistance but not avaialble*/
-    case api_NOT_STORED
+    case api_NOT_STORED(tag: String?)
     /** when trying to load API from persistance but unloadable by version*/
-    case api_UNLOADABLE
+    case api_UNLOADABLE(tag: String?, storedVersion: String?, minimumVersion: String)
     /** when trying to load API from persistance but it does not have correct instance*/
     case invalid_STORED_API
     /** when trying to access Gateway but user is not logged in*/

ThingIFSDK/ThingIFSDKTests/IoTCloudSDKTests.swift
@@ -606,8 +606,9 @@ class ThingIFSDKTests: SmallTestBase {
         let setting = TestSetting()
         let app = setting.app
         let owner = setting.owner
+        let tagName = "testLoadFromStoredInstanceLowerSDKVersion"
 
-        let api = ThingIFAPIBuilder(app:app, owner:owner).build()
+        let api = ThingIFAPIBuilder(app:app, owner:owner, tag:tagName).build()
 
         setMockResponse4Onboard("access-token-00000001", thingID: "th.00000001", setting: setting)
         api.onboard("vendor-0001", thingPassword: "password1", thingType: "smart-light", thingProperties: nil) { ( target, error) -> Void in
@@ -623,7 +624,7 @@ class ThingIFSDKTests: SmallTestBase {
         }
 
         let baseKey = "ThingIFAPI_INSTANCE"
-        let versionKey = "ThingIFAPI_VERSION"
+        let versionKey = "ThingIFAPI_VERSION" + "_\(tagName)"
         if let tempdict = UserDefaults.standard.object(forKey: baseKey) as? NSDictionary {
             let dict  = tempdict.mutableCopy() as! NSMutableDictionary
             dict[versionKey] = "0.0.0"
@@ -631,14 +632,16 @@ class ThingIFSDKTests: SmallTestBase {
             UserDefaults.standard.synchronize()
         }
 
-        XCTAssertThrowsError(try ThingIFAPI.loadWithStoredInstance()) { error in
-            switch error {
-            case ThingIFError.api_UNLOADABLE:
-                // Succeed.
-                break
-            default:
-                XCTFail("Unexpected exception throwed.")
-            }
+        do {
+            let loaded = try ThingIFAPI.loadWithStoredInstance(tagName)
+            XCTAssertNotNil(loaded)
+        } catch ThingIFError.api_UNLOADABLE(let tag, let storedVersion, let minimumVersion) {
+            XCTAssertEqual(tagName, tag)
+            XCTAssertEqual("0.0.0", storedVersion)
+            // Please check ThingIFAPI.MINIMUM_LOADABLE_SDK_VERSION if this check failed.
+            XCTAssertEqual("0.13.0", minimumVersion)
+        } catch {
+            XCTFail("Unexpected exception throwed.")
         }
     }
 

KiiPlatform/thing-if-iOSSDK

ThingIFSDK/ThingIFSDK/Gateway/GatewayAPI.swift
@@ -514,9 +514,9 @@ open class GatewayAPI: NSObject, NSCoding {
         if let dict = UserDefaults.standard.object(forKey: baseKey) as? NSDictionary {
             if dict.object(forKey: key) != nil {
 
-                let sdkVersion = dict.object(forKey: versionKey) as? String
-                if isLoadable(sdkVersion) == false {
-                    throw ThingIFError.apiUnloadable
+                let storedSDKVersion = dict.object(forKey: versionKey) as? String
+                if isLoadable(storedSDKVersion) == false {
+                    throw ThingIFError.apiUnloadable(tag: tag, storedVersion: storedSDKVersion, minimumVersion: MINIMUM_LOADABLE_SDK_VERSION)
                 }
 
                 if let data = dict[key] as? Data {
@@ -529,10 +529,10 @@ open class GatewayAPI: NSObject, NSCoding {
                     throw ThingIFError.invalidStoredApi
                 }
             } else {
-                throw ThingIFError.apiNotStored
+                throw ThingIFError.apiNotStored(tag: tag)
             }
         } else {
-            throw ThingIFError.apiNotStored
+            throw ThingIFError.apiNotStored(tag: tag)
         }
     }
 

ThingIFSDK/ThingIFSDK/ThingIFAPI.swift
@@ -10,11 +10,11 @@ open class ThingIFAPI: NSObject, NSCoding {
 
     private static let SHARED_NSUSERDEFAULT_KEY_INSTANCE = "ThingIFAPI_INSTANCE"
     private static func getStoredInstanceKey(_ tag : String?) -> String{
-        return SHARED_NSUSERDEFAULT_KEY_INSTANCE + (tag == nil ? "" : "_\(tag)")
+        return SHARED_NSUSERDEFAULT_KEY_INSTANCE + (tag == nil ? "" : "_\(tag!)")
     }
     fileprivate static let SHARED_NSUSERDEFAULT_SDK_VERSION_KEY = "ThingIFAPI_VERSION"
     fileprivate static func getStoredSDKVersionKey(_ tag : String?) -> String{
-        return SHARED_NSUSERDEFAULT_SDK_VERSION_KEY + (tag == nil ? "" : "_\(tag)")
+        return SHARED_NSUSERDEFAULT_SDK_VERSION_KEY + (tag == nil ? "" : "_\(tag!)")
     }
     fileprivate static let MINIMUM_LOADABLE_SDK_VERSION = "0.13.0"
 
@@ -812,9 +812,9 @@ open class ThingIFAPI: NSObject, NSCoding {
         {
             if dict.object(forKey: key) != nil {
 
-                let sdkVersion = dict.object(forKey: versionKey) as? String
-                if isLoadable(sdkVersion) == false {
-                    throw ThingIFError.apiUnloadable
+                let storedSDKVersion = dict.object(forKey: versionKey) as? String
+                if isLoadable(storedSDKVersion) == false {
+                    throw ThingIFError.apiUnloadable(tag: tag, storedVersion: storedSDKVersion, minimumVersion: MINIMUM_LOADABLE_SDK_VERSION)
                 }
 
                 if let data = dict[key] as? Data {
@@ -827,10 +827,10 @@ open class ThingIFAPI: NSObject, NSCoding {
                     throw ThingIFError.invalidStoredApi
                 }
             } else {
-                throw ThingIFError.apiNotStored
+                throw ThingIFError.apiNotStored(tag: tag)
             }
         }else{
-            throw ThingIFError.apiNotStored
+            throw ThingIFError.apiNotStored(tag: tag)
         }
     }
     /** Save this instance

ThingIFSDK/ThingIFSDK/ThingIFError.swift
@@ -29,9 +29,9 @@ public enum ThingIFError : Error {
     /** where target not found */
     case targetNotAvailable
     /** when trying to load API from persistance but not avaialble*/
-    case apiNotStored
+    case apiNotStored(tag: String?)
     /** when trying to load API from persistance but unloadable by version*/
-    case apiUnloadable
+    case apiUnloadable(tag: String?, storedVersion: String?, minimumVersion: String)
     /** when trying to load API from persistance but it does not have correct instance*/
     case invalidStoredApi
     /** when trying to access Gateway but user is not logged in*/

ThingIFSDK/ThingIFSDKTests/IoTCloudSDKTests.swift
@@ -606,8 +606,9 @@ class ThingIFSDKTests: SmallTestBase {
         let setting = TestSetting()
         let app = setting.app
         let owner = setting.owner
+        let tagName = "testLoadFromStoredInstanceLowerSDKVersion"
 
-        let api = ThingIFAPIBuilder(app:app, owner:owner).build()
+        let api = ThingIFAPIBuilder(app:app, owner:owner, tag:tagName).build()
 
         setMockResponse4Onboard("access-token-00000001", thingID: "th.00000001", setting: setting)
         api.onboard("vendor-0001", thingPassword: "password1", thingType: "smart-light", thingProperties: nil) { ( target, error) -> Void in
@@ -623,7 +624,7 @@ class ThingIFSDKTests: SmallTestBase {
         }
 
         let baseKey = "ThingIFAPI_INSTANCE"
-        let versionKey = "ThingIFAPI_VERSION"
+        let versionKey = "ThingIFAPI_VERSION" + "_\(tagName)"
         if let tempdict = UserDefaults.standard.object(forKey: baseKey) as? NSDictionary {
             let dict  = tempdict.mutableCopy() as! NSMutableDictionary
             dict[versionKey] = "0.0.0"
@@ -631,14 +632,16 @@ class ThingIFSDKTests: SmallTestBase {
             UserDefaults.standard.synchronize()
         }
 
-        XCTAssertThrowsError(try ThingIFAPI.loadWithStoredInstance()) { error in
-            switch error {
-            case ThingIFError.apiUnloadable:
-                // Succeed.
-                break
-            default:
-                XCTFail("Unexpected exception throwed.")
-            }
+        do {
+            let loaded = try ThingIFAPI.loadWithStoredInstance(tagName)
+            XCTAssertNotNil(loaded)
+        } catch ThingIFError.apiUnloadable(let tag, let storedVersion, let minimumVersion) {
+            XCTAssertEqual(tagName, tag)
+            XCTAssertEqual("0.0.0", storedVersion)
+            // Please check ThingIFAPI.MINIMUM_LOADABLE_SDK_VERSION if this check failed.
+            XCTAssertEqual("0.13.0", minimumVersion)
+        } catch {
+            XCTFail("Unexpected exception throwed.")
         }
     }
 