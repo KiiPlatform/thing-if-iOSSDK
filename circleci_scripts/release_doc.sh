7f9090a8bcc459a7be54ef9f07d7445a96fb1db5
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-11-25 16:39:02 +0900

    Deployment section to release SDK is added.

---
+++ circleci_scripts/release_doc.sh
@@ -0,0 +1,41 @@
+#! bin/bash
+
+declare -a uploadhosts=($DOC_HOST1 $DOC_HOST2)
+
+basedir="/ext/ebs/references/ios/thing-if"
+version=$(cd thing-if-iOSSDK/ThingIFSDK; agvtool what-version | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+')
+
+updir="$basedir/$version"
+latestdir="$basedir/latest"
+echo ""
+for host in "${uploadhosts[@]}"; do
+  uptarget="$host:$updir"
+  echo "Uploading to : $host"
+  rsync -rlptDe "ssh -o StrictHostKeyChecking=no" --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after ~/thing-if-iOS-Deployment/thing-if-iOSSDK/ThingIFSDK/Documentation/docs/ "$uptarget"
+
+  # check command exit code
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    echo "Faild when uploading doc to : $uptarget"
+    exit $exitCode
+  fi
+
+  ssh -o StrictHostKeyChecking=no "$host" "rm $latestdir"
+  # check command result
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    echo "Faild when removing older doc to : $uptarget"
+    exit $exitCode
+  fi
+
+  ssh -o StrictHostKeyChecking=no "$host" "ln -s $updir $latestdir"
+  # check command result
+  exitCode=$?
+  if [ $exitCode -ne 0 ]; then
+    echo "Faild when releasing new doc to : $uptarget"
+    exit $exitCode
+  fi
+
+done
+
+echo "All uploads have completed!"
