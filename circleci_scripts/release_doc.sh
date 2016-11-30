bb9f87eb46867b49bfb188d33bba97ce45ffe47f
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2016-11-30 18:29:47 +0900

    release_doc.sh is fixed.

--- circleci_scripts/release_doc.sh
+++ circleci_scripts/release_doc.sh
@@ -10,29 +10,29 @@ latestdir="$basedir/latest"
 echo ""
 for host in "${uploadhosts[@]}"; do
   uptarget="$host:$updir"
-  echo "Uploading to : $host"
-  rsync -rlptDe "ssh -o StrictHostKeyChecking=no" --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after ~/thing-if-iOSSDK/ThingIFSDK/Documentation/docs/ "$uptarget"
+  echo "Uploading..."
+  rsync -rlptDe "ssh -o StrictHostKeyChecking=no" --chmod=u+rw,g+r,o+r --chmod=Da+x --delete-after ~/thing-if-iOSSDK/ThingIFSDK/Documentation/docs/ "$uptarget" > /dev/null 2>&1
 
   # check command exit code
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
-    echo "Faild when uploading doc to : $uptarget"
+    echo "Faild when uploading doc"
     exit $exitCode
   fi
 
-  ssh -o StrictHostKeyChecking=no "$host" "rm $latestdir"
+  ssh -o StrictHostKeyChecking=no "$host" "rm $latestdir" > /dev/null 2>&1
   # check command result
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
-    echo "Faild when removing older doc to : $uptarget"
+    echo "Faild when removing older doc"
     exit $exitCode
   fi
 
-  ssh -o StrictHostKeyChecking=no "$host" "ln -s $updir $latestdir"
+  ssh -o StrictHostKeyChecking=no "$host" "ln -s $updir $latestdir" > /dev/null 2>&1
   # check command result
   exitCode=$?
   if [ $exitCode -ne 0 ]; then
-    echo "Faild when releasing new doc to : $uptarget"
+    echo "Faild when releasing new doc"
     exit $exitCode
   fi
 
