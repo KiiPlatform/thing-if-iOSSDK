56500fc78707904181f41b43869e3c7a34590f76
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-01-23 16:35:18 +0900

    Add base clauses.

--- ThingIFSDK/ThingIFSDK/BaseClause.swift
+++ ThingIFSDK/ThingIFSDK/BaseClause.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+
+/** Protocol for any clauses class. */
+public protocol BaseClause: NSCoding {
+
+    /** Get Clause as Dictionary instance
+
+    - Returns: a Dictionary instance.
+    */
+    func makeDictionary() -> [ String : Any ]
+
+}
+
+/** Protocol for equals clause class */
+public protocol BaseEquals: BaseClause {
+
+    /** Name of a field. */
+    var field: String { get }
+    /** Value of a field. */
+    var value: Any { get }
+}
+
+/** Protocol for not equals clause class */
+public protocol BaseNotEquals: BaseClause {
+
+    /** Type of a contained instance */
+    associatedtype EqualClauseType: BaseEquals
+
+    /** Contained equals clause object. */
+    var equals: EqualClauseType { get }
+}
+
+/** Protocol for range clause. */
+public protocol BaseRange: BaseClause {
+
+    /** Name of a field. */
+    var field: String { get }
+    /** lower limit for an instance. */
+    var lower: (limit: AnyObject, included: Bool)? { get }
+    /** upper limit for an instance. */
+    var upper: (limit: AnyObject, included: Bool)? { get }
+}
+
+/** Protocol for and clause. */
+public protocol BaseAnd: BaseClause {
+
+    /** Type of contained instances */
+    associatedtype ClausesType: BaseClause
+
+    /** Contained clauses. */
+    var clauses: [ClausesType] { get }
+
+    /** Add a clause. */
+    func add(_ clause: ClausesType) -> Void
+}
+
+
+/** Protocol for or clause. */
+public protocol BaseOr: BaseClause {
+
+    /** Type of contained instances */
+    associatedtype ClausesType: BaseClause
+
+    /** Contained clauses. */
+    var clauses: [ClausesType] { get }
+
+    /** Add a clause. */
+    func add(_ clause: ClausesType) -> Void
+}
