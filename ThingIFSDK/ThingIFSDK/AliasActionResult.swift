e7e52aaa11f75b0e5f2729c812639db464f85c99
naoki.akimoto <naoki.akimoto@muraoka-design.com>
2017-02-07 20:09:29 +0900

    implemant and test AliasActionResult.

--- ThingIFSDK/ThingIFSDK/AliasActionResult.swift
+++ ThingIFSDK/ThingIFSDK/AliasActionResult.swift
@@ -17,14 +17,33 @@ open class AliasActionResult: NSCoding {
     open let results: [ActionResult]
 
     internal init(_ alias: String, results: [ActionResult]) {
-        fatalError("TODO: implement me.")
+        self.alias = alias
+        self.results = results
     }
 
     public required convenience init?(coder aDecoder: NSCoder) {
-        fatalError("TODO: implement me.")
+        let alias = aDecoder.decodeObject(forKey: "alias") as! String
+        let count = aDecoder.decodeInteger(forKey: "count")
+        var results: [ActionResult] = []
+        for i in 0..<count {
+            let data = aDecoder.decodeObject(forKey: String(i))
+            let decoder: NSKeyedUnarchiver = NSKeyedUnarchiver(forReadingWith: data as! Data);
+            let result = ActionResult(coder: decoder)!
+            decoder.finishDecoding()
+            results.append(result)
+        }
+        self.init(alias, results: results)
     }
 
     public func encode(with aCoder: NSCoder) {
-        fatalError("TODO: implement me.")
+        aCoder.encode(self.alias, forKey: "alias")
+        aCoder.encode(self.results.count, forKey: "count")
+        for i in 0..<self.results.count {
+            let data = NSMutableData(capacity: 1024)!
+            let coder = NSKeyedArchiver(forWritingWith: data)
+            self.results[i].encode(with: coder)
+            coder.finishEncoding()
+            aCoder.encode(data, forKey: String(i))
+        }
     }
 }
