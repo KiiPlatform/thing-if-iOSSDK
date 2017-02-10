ad282940a19f21961d651f254562dfe5e543f6e9
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-02-10 13:12:08 +0900

    Implement encoder and decoder for NSCoding.

--- ThingIFSDK/ThingIFSDK/AliasActionResult.swift
+++ ThingIFSDK/ThingIFSDK/AliasActionResult.swift
@@ -22,28 +22,13 @@ open class AliasActionResult: NSCoding {
     }
 
     public required convenience init?(coder aDecoder: NSCoder) {
-        let alias = aDecoder.decodeObject(forKey: "alias") as! String
-        let count = aDecoder.decodeInteger(forKey: "count")
-        var results: [ActionResult] = []
-        for i in 0..<count {
-            let data = aDecoder.decodeObject(forKey: String(i))
-            let decoder: NSKeyedUnarchiver = NSKeyedUnarchiver(forReadingWith: data as! Data);
-            let result = ActionResult(coder: decoder)!
-            decoder.finishDecoding()
-            results.append(result)
-        }
-        self.init(alias, results: results)
+        self.init(
+          aDecoder.decodeObject(forKey: "alias") as! String,
+          results: aDecoder.decodeNSCodingArray(forKey: "results")!)
     }
 
     public func encode(with aCoder: NSCoder) {
         aCoder.encode(self.alias, forKey: "alias")
-        aCoder.encode(self.results.count, forKey: "count")
-        for i in 0..<self.results.count {
-            let data = NSMutableData(capacity: 1024)!
-            let coder = NSKeyedArchiver(forWritingWith: data)
-            self.results[i].encode(with: coder)
-            coder.finishEncoding()
-            aCoder.encode(data, forKey: String(i))
-        }
+        aCoder.encodeNSCodingArray(self.results, forKey: "results")
     }
 }
