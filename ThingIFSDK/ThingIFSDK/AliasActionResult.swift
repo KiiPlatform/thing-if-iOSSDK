2a9c09b5d97168a275b7c7f1305da73ffec72bd4
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-02 13:25:40 +0900

    AliasActionResult becomes struct.

--- ThingIFSDK/ThingIFSDK/AliasActionResult.swift
+++ ThingIFSDK/ThingIFSDK/AliasActionResult.swift
@@ -9,26 +9,16 @@
 import Foundation
 
 /** Result of action for an alias. */
-open class AliasActionResult: NSObject, NSCoding {
+public struct AliasActionResult {
 
     /** Name of an alias. */
-    open let alias: String
+    public let alias: String
     /** Results of actions for an alias. */
-    open let results: [ActionResult]
+    public let results: [ActionResult]
 
     internal init(_ alias: String, results: [ActionResult]) {
         self.alias = alias
         self.results = results
     }
 
-    public required convenience init?(coder aDecoder: NSCoder) {
-        self.init(
-          aDecoder.decodeObject(forKey: "alias") as! String,
-          results: aDecoder.decodeObject(forKey: "results") as! [ActionResult])
-    }
-
-    public func encode(with aCoder: NSCoder) {
-        aCoder.encode(self.alias, forKey: "alias")
-        aCoder.encode(self.results, forKey: "results")
-    }
 }
--- ThingIFSDK/ThingIFSDKTests/AliasActionResultTests.swift
+++ ThingIFSDK/ThingIFSDKTests/AliasActionResultTests.swift
@@ -28,29 +28,4 @@ class AliasActionResultTests: SmallTestBase {
         verifyArray(results, actual: target.results)
     }
 
-    func testCoding() {
-        let results = [ActionResult(true, actionName: "dummy", data: nil, errorMessage: nil)]
-        let original = AliasActionResult("dummy", results: results)
-        XCTAssertNotNil(original)
-        XCTAssertEqual(original.alias, "dummy")
-        verifyArray(results, actual: original.results)
-
-        let data = NSMutableData(capacity: 1024)!
-        let coder = NSKeyedArchiver(forWritingWith: data)
-        original.encode(with: coder)
-        coder.finishEncoding()
-
-        let decoder: NSKeyedUnarchiver = NSKeyedUnarchiver(forReadingWith: data as Data);
-        let decoded = AliasActionResult(coder: decoder);
-        decoder.finishDecoding();
-
-        XCTAssertNotNil(decoded)
-        XCTAssertEqual(decoded!.alias, original.alias)
-        XCTAssertNotNil(decoded!.results)
-        XCTAssertEqual(decoded!.results.count, results.count)
-        XCTAssertEqual(decoded!.results[0].succeeded, results[0].succeeded)
-        XCTAssertEqual(decoded!.results[0].actionName, results[0].actionName)
-        XCTAssertNil(decoded!.results[0].data)
-        XCTAssertNil(decoded!.results[0].errorMessage)
-    }
 }
