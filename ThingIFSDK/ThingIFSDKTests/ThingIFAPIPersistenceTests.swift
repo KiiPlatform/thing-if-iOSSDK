33bb54412d303ee59c06486404e7f7bb816f8130
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-13 14:38:25 +0900

    Implement a test.

--- ThingIFSDK/ThingIFSDKTests/ThingIFAPIPersistenceTests.swift
+++ ThingIFSDK/ThingIFSDKTests/ThingIFAPIPersistenceTests.swift
@@ -61,10 +61,9 @@ class ThingIFAPIPersistenceTests: SmallTestBase {
           vendorThingID: "vendor-0001",
           thingPassword: "password1",
           options: OnboardWithVendorThingIDOptions("smart-light")) {
-            ( target, error) -> Void in
-            if error != nil{
-                XCTFail("should success")
-            }
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
             expectation.fulfill()
         }
         self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
@@ -83,11 +82,9 @@ class ThingIFAPIPersistenceTests: SmallTestBase {
           vendorThingID: "vendor-0002",
           thingPassword: "password2",
           options: OnboardWithVendorThingIDOptions("smart-light")) {
-            ( target, error) -> Void in
-
-            if error != nil{
-                XCTFail("should success")
-            }
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
             expectation.fulfill()
         }
         self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
@@ -106,10 +103,9 @@ class ThingIFAPIPersistenceTests: SmallTestBase {
           vendorThingID: "vendor-0002",
           thingPassword: "password2",
           options: OnboardWithVendorThingIDOptions("smart-light")) {
-            ( target, error) -> Void in
-            if error != nil{
-                XCTFail("should success")
-            }
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
             expectation.fulfill()
         }
         self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
@@ -136,6 +132,106 @@ class ThingIFAPIPersistenceTests: SmallTestBase {
             error in
             XCTAssertEqual(
               ThingIFError.apiNotStored(tag: "tag2"),
+              error as? ThingIFError)
+        }
+    }
+
+    func testSavedInstanceWithOnboard() throws {
+        let tags = ["tag1", "tag2"]
+        let setting = TestSetting()
+        let app = setting.app
+        let owner = setting.owner
+
+        let api1 = ThingIFAPI(app, owner:owner)
+        let api2 = ThingIFAPI(app, owner:owner, tag:tags[0])
+        let api3 = ThingIFAPI(app, owner:owner, tag:tags[1])
+
+        var expectation = self.expectation(
+          description: "testSavedInstanceWithOnboard")
+        try setMockResponse4Onboard(
+          "access-token-00000001",
+          thingID: "th.00000001",
+          setting: setting)
+        api1.onboardWith(
+          vendorThingID: "vendor-0001",
+          thingPassword: "password1",
+          options: OnboardWithVendorThingIDOptions("smart-light")) {
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            if error != nil {
+                XCTFail("execution timeout")
+            }
+        }
+
+        expectation = self.expectation(
+          description: "testSavedInstanceWithOnboard")
+        try setMockResponse4Onboard(
+          "access-token-00000002",
+          thingID: "th.00000002",
+          setting: setting)
+        api2.onboardWith(
+          vendorThingID: "vendor-0002",
+          thingPassword: "password2",
+          options: OnboardWithVendorThingIDOptions("smart-light")) {
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            if error != nil {
+                XCTFail("execution timeout")
+            }
+        }
+
+        expectation = self.expectation(
+          description: "testSavedInstanceWithOnboard")
+        try setMockResponse4Onboard(
+          "access-token-00000003",
+          thingID: "th.00000003",
+          setting: setting)
+        api3.onboardWith(
+          vendorThingID: "vendor-0002",
+          thingPassword: "password2",
+          options: OnboardWithVendorThingIDOptions("smart-light")) {
+            (target, error) -> Void in
+            XCTAssertNotNil(target)
+            XCTAssertNil(error)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            if error != nil {
+                XCTFail("execution timeout")
+            }
+        }
+
+        XCTAssertEqual(api1, try ThingIFAPI.loadWithStoredInstance())
+        XCTAssertEqual(api2, try ThingIFAPI.loadWithStoredInstance(tags[0]))
+        XCTAssertEqual(api3, try ThingIFAPI.loadWithStoredInstance(tags[1]))
+
+        ThingIFAPI.removeStoredInstances(nil)
+        XCTAssertThrowsError(try ThingIFAPI.loadWithStoredInstance()) {
+            error in
+            XCTAssertEqual(
+              ThingIFError.apiNotStored(tag: nil),
+              error as? ThingIFError)
+        }
+        ThingIFAPI.removeStoredInstances(tags[0])
+        XCTAssertThrowsError(try ThingIFAPI.loadWithStoredInstance(tags[0])) {
+            error in
+            XCTAssertEqual(
+              ThingIFError.apiNotStored(tag: tags[0]),
+              error as? ThingIFError)
+        }
+        ThingIFAPI.removeStoredInstances(tags[1])
+        XCTAssertThrowsError(try ThingIFAPI.loadWithStoredInstance(tags[1])) {
+            error in
+            XCTAssertEqual(
+              ThingIFError.apiNotStored(tag: tags[1]),
               error as? ThingIFError)
         }
     }
