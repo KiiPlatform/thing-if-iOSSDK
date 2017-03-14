e24215846b945d332a10a881c7303128f1ff11a8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-14 14:32:35 +0900

    Fix PendingEndNode according to thing-if AndroidSDK.

--- ThingIFSDK/ThingIFSDK/Gateway/PendingEndNode.swift
+++ ThingIFSDK/ThingIFSDK/Gateway/PendingEndNode.swift
@@ -8,17 +8,15 @@
 import Foundation
 
 public struct PendingEndNode {
-    private static let KEY_VENDORTHINGID = "vendorThingID"
-    private static let KEY_THINGPROPERTIES = "thingProperties"
 
     /** Vendor thing ID. */
-    public let vendorThingID: String?
+    public let vendorThingID: String
+    /** Thing type. */
+    public let thingType: String?
     /** Thing properties. */
     public let thingProperties: [String : Any]?
-    /** Thing type. */
-    public var thingType: String? {
-        return self.thingProperties?["_thingType"] as? String
-    }
+    /** Firmware version. */
+    public let firmwareVersion: String?
 
     /** Initialize `ActionResult`.
 
@@ -29,16 +27,28 @@ public struct PendingEndNode {
      - Parameters vendorThingID: Vendor thing ID.
      - Parameters thingProperties: Thing properties.
      */
-    public init(_ vendorThingID: String?, thingProperties: [String : Any]?) {
+    public init(
+      _ vendorThingID: String,
+      thingType: String? = nil,
+      thingProperties: [String : Any]? = nil,
+      firmwareVersion: String? = nil)
+    {
         self.vendorThingID = vendorThingID
+        self.thingType = thingType
         self.thingProperties = thingProperties
+        self.firmwareVersion = firmwareVersion
     }
 
-    internal init(_ json: [String : Any])
-    {
-        self.init(
-          json[PendingEndNode.KEY_VENDORTHINGID] as? String,
-          thingProperties: json[PendingEndNode.KEY_THINGPROPERTIES]
-            as? [String : Any])
+}
+
+extension PendingEndNode: ToJsonObject {
+
+    internal func makeJsonObject() -> [String : Any]{
+        var retval: [String : Any] =
+          ["endNodeVendorThingID" : self.vendorThingID ]
+        retval["endNodeThingType"] = self.thingType
+        retval["endNodeThingProperties"] = self.thingProperties
+        retval["endNodeFirmwareVersion"] = self.firmwareVersion
+        return retval
     }
 }
