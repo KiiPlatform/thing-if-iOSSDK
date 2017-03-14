5789f24d1c2f75b1324b2f7471e2d11d41d34b8c
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-14 17:03:31 +0900

    Implement 2 tests.

--- ThingIFSDK/ThingIFSDKTests/ThingIFAPIOnboardEndNodeTests.swift
+++ ThingIFSDK/ThingIFSDKTests/ThingIFAPIOnboardEndNodeTests.swift
@@ -201,4 +201,188 @@ class ThingIFAPIOnboardEndNodeTests: SmallTestBase {
             XCTAssertNil(error, "execution timeout")
         }
     }
+
+    func testOnboardEndnodeWithGateway404Error() throws {
+        let expectation = self.expectation(
+          description: "testOnboardEndnodeWithGatewayThingID404Error")
+        let setting = TestSetting()
+        let gatewayThingID = "dummyGatewayThingID"
+        let vendorThingID = "dummyVendorThingID"
+        let password = "dummyPassword"
+        let thingProperties = [
+          "_thingType": setting.thingType,
+          "manufacture": "kii"
+        ]
+        let firmwareVersion = "dummyFirmwareVersion"
+
+        // perform onboarding
+        setting.api.target = Gateway(
+          gatewayThingID,
+          vendorThingID: vendorThingID)
+
+        // verify request
+        sharedMockSession.requestVerifier = makeRequestVerifier() {(request) in
+            XCTAssertEqual(request.httpMethod, "POST")
+
+            // verify path
+            XCTAssertEqual(
+              "\(setting.api.baseURL)/thing-if/apps/\(setting.app.appID)/onboardings",
+              request.url!.absoluteString)
+
+            //verify header
+            XCTAssertEqual(
+              [
+                "X-Kii-AppID": setting.app.appID,
+                "X-Kii-AppKey": setting.app.appKey,
+                "X-Kii-SDK": SDKVersion.sharedInstance.kiiSDKHeader,
+                "Authorization": "Bearer \(setting.owner.accessToken)",
+                "Content-Type": "application/vnd.kii.OnboardingEndNodeWithGatewayThingID+json"
+              ],
+              request.allHTTPHeaderFields!)
+
+            //verify body
+            XCTAssertEqual(
+              [
+                "owner": setting.owner.typedID.toString(),
+                "gatewayThingID": gatewayThingID,
+                "endNodeVendorThingID": vendorThingID,
+                "endNodePassword": password,
+                "endNodeThingType": setting.thingType,
+                "endNodeThingProperties": thingProperties,
+                "endNodeFirmwareVersion": firmwareVersion
+              ] as NSDictionary,
+              try JSONSerialization.jsonObject(
+                with: request.httpBody!,
+                options: JSONSerialization.ReadingOptions.allowFragments)
+                as? NSDictionary)
+        }
+
+        // mock response
+        sharedMockSession.mockResponse = (
+          nil,
+          HTTPURLResponse(
+            url: URL(string:setting.app.baseURL)!,
+            statusCode: 404,
+            httpVersion: nil,
+            headerFields: nil),
+          nil)
+        iotSession = MockSession.self
+
+        setting.api.onboard(
+          PendingEndNode(
+            vendorThingID,
+            thingType: setting.thingType,
+            thingProperties: thingProperties,
+            firmwareVersion: firmwareVersion),
+          endnodePassword: password) { (endNode, error) -> Void in
+            XCTAssertNil(endNode)
+            XCTAssertNotNil(error)
+            XCTAssertNil(endNode)
+            XCTAssertEqual(
+              ThingIFError.errorResponse(
+                required: ErrorResponse(
+                  404,
+                  errorCode: "",
+                  errorMessage: "")),
+              error)
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 20.0) { (error) -> Void in
+            XCTAssertNil(error, "execution timeout")
+        }
+    }
+
+    func testOnboardEndnodeWithGateway500Error() throws {
+        let expectation = self.expectation(
+          description: "testOnboardEndnodeWithGatewayThingID500Error")
+        let setting = TestSetting()
+        let gatewayThingID = "dummyGatewayThingID"
+        let vendorThingID = "dummyVendorThingID"
+        let password = "dummyPassword"
+        let thingProperties = [
+          "_thingType": setting.thingType,
+          "manufacture": "kii"
+        ]
+        let firmwareVersion = "dummyFirmwareVersion"
+
+        // perform onboarding
+        setting.api.target = Gateway(
+          gatewayThingID,
+          vendorThingID: vendorThingID)
+
+        // verify request
+        sharedMockSession.requestVerifier = makeRequestVerifier() {(request) in
+            XCTAssertEqual(request.httpMethod, "POST")
+
+            // verify path
+            XCTAssertEqual(
+              "\(setting.api.baseURL)/thing-if/apps/\(setting.app.appID)/onboardings",
+              request.url!.absoluteString)
+
+            //verify header
+            XCTAssertEqual(
+              [
+                "X-Kii-AppID": setting.app.appID,
+                "X-Kii-AppKey": setting.app.appKey,
+                "X-Kii-SDK": SDKVersion.sharedInstance.kiiSDKHeader,
+                "Authorization": "Bearer \(setting.owner.accessToken)",
+                "Content-Type": "application/vnd.kii.OnboardingEndNodeWithGatewayThingID+json"
+              ],
+              request.allHTTPHeaderFields!)
+
+            //verify body
+            XCTAssertEqual(
+              [
+                "owner": setting.owner.typedID.toString(),
+                "gatewayThingID": gatewayThingID,
+                "endNodeVendorThingID": vendorThingID,
+                "endNodePassword": password,
+                "endNodeThingType": setting.thingType,
+                "endNodeThingProperties": thingProperties,
+                "endNodeFirmwareVersion": firmwareVersion
+              ] as NSDictionary,
+              try JSONSerialization.jsonObject(
+                with: request.httpBody!,
+                options: JSONSerialization.ReadingOptions.allowFragments)
+                as? NSDictionary)
+        }
+
+        // mock response
+        sharedMockSession.mockResponse = (
+          nil,
+          HTTPURLResponse(
+            url: URL(string:setting.app.baseURL)!,
+            statusCode: 500,
+            httpVersion: nil,
+            headerFields: nil),
+          nil)
+        iotSession = MockSession.self
+
+        setting.api.onboard(
+          PendingEndNode(
+            vendorThingID,
+            thingType: setting.thingType,
+            thingProperties: thingProperties,
+            firmwareVersion: firmwareVersion),
+          endnodePassword: password) { (endNode, error) -> Void in
+            XCTAssertNil(endNode)
+            XCTAssertNotNil(error)
+            XCTAssertNil(endNode)
+            XCTAssertEqual(
+              ThingIFError.errorResponse(
+                required: ErrorResponse(
+                  500,
+                  errorCode: "",
+                  errorMessage: "")),
+              error)
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: 20.0) { (error) -> Void in
+            XCTAssertNil(error, "execution timeout")
+        }
+    }
+
+
 }
