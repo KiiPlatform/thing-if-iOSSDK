ebead50827a7c3df3d868fd57aa240c4fbb54811
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-15 12:13:11 +0900

    Add a method to put http request operation to operation queue.

--- ThingIFSDK/ThingIFSDK/ThingIFAPI+OnBoard.swift
+++ ThingIFSDK/ThingIFSDK/ThingIFAPI+OnBoard.swift
@@ -140,62 +140,49 @@ extension ThingIFAPI {
       vendorThingID: String? = nil,
       completionHandler: @escaping (Target?, ThingIFError?) -> Void) -> Void
     {
-        let data: Data
-        do {
-            data = try JSONSerialization.data(
-              withJSONObject: requestBody,
-              options: JSONSerialization.WritingOptions(rawValue: 0))
-        } catch {
-            kiiSevereLog("ThingIFError.JSON_PARSE_ERROR")
-            completionHandler(nil, ThingIFError.jsonParseError)
-            return
-        }
+        self.operationQueue.addHttpRequestOperation(
+          .post,
+          url: "\(self.baseURL)/thing-if/apps/\(self.appID)/onboardings",
+          requestHeader:
+            self.defaultHeader + ["Content-Type" : mediaType.rawValue],
+          requestBody: requestBody,
+          failureBeforeExecutionHandler: { completionHandler(nil, $0) }) {
+            response, error in
 
-        operationQueue.addOperation(
-          IoTRequestOperation(
-            request: buildDefaultRequest(
-              .post,
-              urlString:
-                "\(self.baseURL)/thing-if/apps/\(self.appID)/onboardings",
-              requestHeaderDict:
-                self.defaultHeader + ["Content-Type" : mediaType.rawValue],
-              requestBodyData: data) { response, error in
-                var target: Target?
-                var error2 = error
-                if error == nil {
-                    do {
-                        /* TODO:
-                         Idealy, server should send vendorThingID as
-                         response of onboarding, and SDK should use
-                         the received vendorThingID. However, current
-                         server implementation does not send
-                         vendorThingID. So we used IDString if it is
-                         vendorThingID, otherwise we set it empty
-                         string. This behavior should be fixed after
-                         server fixed.
-                        */
-                        self.target = try makeTargetThing(
-                          response!,
-                          layoutPosition: layoutPosition ?? .standalone,
-                          vendorThingID: vendorThingID)
-                        self.saveToUserDefault()
-                        target = self.target
-                    } catch (ThingIFError.jsonParseError) {
-                        // This is unexpected case.
-                        // If response body is not unexpected format,
-                        // this error is thrown
-                        kiiSevereLog("unexpected error")
-                        error2 = ThingIFError.jsonParseError
-                    } catch {
-                        // This case must not happen.
-                        kiiSevereLog("must not happen")
-                        error2 = ThingIFError.jsonParseError
-                    }
+            var target: Target?
+            var error2 = error
+            if error == nil {
+                do {
+                    /* TODO:
+                     Idealy, server should send vendorThingID as
+                     response of onboarding, and SDK should use
+                     the received vendorThingID. However, current
+                     server implementation does not send
+                     vendorThingID. So we used IDString if it is
+                     vendorThingID, otherwise we set it empty
+                     string. This behavior should be fixed after
+                     server fixed.
+                     */
+                    self.target = try makeTargetThing(
+                      response!,
+                      layoutPosition: layoutPosition ?? .standalone,
+                      vendorThingID: vendorThingID)
+                    self.saveToUserDefault()
+                    target = self.target
+                } catch (ThingIFError.jsonParseError) {
+                    // This is unexpected case.
+                    // If response body is not unexpected format,
+                    // this error is thrown
+                    kiiSevereLog("unexpected error")
+                    error2 = ThingIFError.jsonParseError
+                } catch {
+                    // This case must not happen.
+                    kiiSevereLog("must not happen")
+                    error2 = ThingIFError.jsonParseError
                 }
-                DispatchQueue.main.async { completionHandler(target, error2) }
             }
-          )
-        )
+            DispatchQueue.main.async { completionHandler(target, error2) }
+        }
     }
 
 }
