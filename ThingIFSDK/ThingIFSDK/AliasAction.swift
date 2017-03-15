12197e11836bb88f20ce6d636acb751df7d119f8
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-15 18:59:02 +0900

    Add Action.

--- ThingIFSDK/ThingIFSDK/AliasAction.swift
+++ ThingIFSDK/ThingIFSDK/AliasAction.swift
@@ -16,19 +16,16 @@ public struct AliasAction {
     /** Name of an alias. */
     public let alias: String
     /** Action of this alias. */
-    public let actions: [(name: String, value: Any)]
+    public let actions: [Action]
 
     // MARK: - Initializing CommandForm instance.
     /** Initializer of AliasAction instance.
 
      - Parameter alias: Name of an alias.
-     - Parameter actions: Actions of this alias.
-       - name: name of an action
-       - value: value of an action. This must be number, bool, string
-         or json compatible array or dictionary
+     - Parameter actions: Actions of this alias. must not be empty.
      */
 
-    public init(_ alias: String, actions: [(name: String, value: Any)]) {
+    public init(_ alias: String, actions: [Action]) {
         self.alias = alias
         self.actions = actions
     }
