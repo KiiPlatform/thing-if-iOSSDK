99e98c70b29c642b3306c79bab8a6d51b3356074
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-17 18:00:48 +0900

    Add Initializer for AggregatedResult.

--- ThingIFSDK/ThingIFSDK/AggregatedResult.swift
+++ ThingIFSDK/ThingIFSDK/AggregatedResult.swift
@@ -28,7 +28,7 @@ public struct AggregatedResult<AggregatedValueType> {
      - Parameters timeRange: Time range of an aggregated result.
      - Parameters aggregatedObjects: Aggregated objectes.
      */
-    public init(
+    fileprivate init(
       _ value: AggregatedValueType?,
       timeRange: TimeRange,
       aggregatedObjects: [HistoryState])
@@ -38,4 +38,34 @@ public struct AggregatedResult<AggregatedValueType> {
         self.aggregatedObjects = aggregatedObjects
     }
 
+}
+
+extension AggregatedResult: FromJsonObject {
+
+    internal init(_ jsonObject: [String : Any]) throws {
+        guard let range = jsonObject["range"] as? [String : Double],
+              let aggregations =
+                jsonObject["aggregations"] as? [[String : Any]] else {
+            throw ThingIFError.jsonParseError
+        }
+
+        if aggregations.count == 0 {
+            kiiSevereLog("Aggregation not found")
+            throw ThingIFError.jsonParseError
+        }
+        if aggregations.count >= 2 {
+            kiiSevereLog("Currently, number of aggregations must be 1")
+        }
+
+        let aggregation = aggregations[0]
+        guard let value = aggregation["value"] as? AggregatedValueType,
+              let object = aggregation["object"] as? [String : Any] else {
+            throw ThingIFError.jsonParseError
+        }
+
+        self.init(
+          value,
+          timeRange: try TimeRange(range),
+          aggregatedObjects: [try HistoryState(object)])
+    }
 }
