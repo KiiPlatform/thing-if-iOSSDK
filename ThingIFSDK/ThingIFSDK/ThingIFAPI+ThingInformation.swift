2322a85f080bcf1fd450506bc8b3d4aeabcea7c1
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-21 15:48:48 +0900

    Move update method.

--- ThingIFSDK/ThingIFSDK/ThingIFAPI+ThingInformation.swift
+++ ThingIFSDK/ThingIFSDK/ThingIFAPI+ThingInformation.swift
@@ -47,4 +47,65 @@ extension ThingIFAPI {
         }
     }
 
+    /** Update the Vendor Thing ID of specified Target.
+
+     - Parameter vendorThingID: New vendor thing id
+     - Parameter password: New password
+     - Parameter completionHandler: A closure to be executed once finished. The closure takes 1 argument: an instance of ThingIFError when failed.
+     */
+    open func update(
+        vendorThingID: String,
+        password: String,
+        completionHandler: @escaping (ThingIFError?)-> Void
+        )
+    {
+        if self.target == nil {
+            completionHandler(ThingIFError.targetNotAvailable)
+            return;
+        }
+        if vendorThingID.isEmpty || password.isEmpty {
+            completionHandler(ThingIFError.unsupportedError)
+            return;
+        }
+
+        let requestURL = "\(self.baseURL)/api/apps/\(self.appID)/things/\(self.target!.typedID.id)/vendor-thing-id"
+
+        // generate header
+        let requestHeaderDict:Dictionary<String, String> = [
+            "x-kii-appid": self.appID,
+            "x-kii-appkey": self.appKey,
+            "authorization": "Bearer \(self.owner.accessToken)",
+            "Content-Type": "application/vnd.kii.VendorThingIDUpdateRequest+json"
+        ]
+
+        // genrate body
+        let requestBodyDict = NSMutableDictionary(dictionary:
+            [
+                "_vendorThingID": vendorThingID,
+                "_password": password
+            ]
+        )
+
+        do {
+            let requestBodyData = try JSONSerialization.data(withJSONObject: requestBodyDict, options: JSONSerialization.WritingOptions(rawValue: 0))
+            // do request
+            let request = buildDefaultRequest(
+                HTTPMethod.PUT,
+                urlString: requestURL,
+                requestHeaderDict: requestHeaderDict,
+                requestBodyData: requestBodyData,
+                completionHandler: { (response, error) -> Void in
+                    DispatchQueue.main.async {
+                        completionHandler(error)
+                    }
+                }
+            )
+            let operation = IoTRequestOperation(request: request)
+            operationQueue.addOperation(operation)
+        } catch(_) {
+            kiiSevereLog("ThingIFError.JSON_PARSE_ERROR")
+            completionHandler(ThingIFError.jsonParseError)
+        }
+    }
+
 }
