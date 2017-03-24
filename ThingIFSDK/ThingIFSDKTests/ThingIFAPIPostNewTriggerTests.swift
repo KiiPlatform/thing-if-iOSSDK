d228e34feb86a76085a047e0022fea6ceab9420f
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-24 15:07:32 +0900

    Implement a test.

---
+++ ThingIFSDK/ThingIFSDKTests/ThingIFAPIPostNewTriggerTests.swift
@@ -0,0 +1,295 @@
+//
+//  ThingIFAPIPostNewTriggerTests.swift
+//  ThingIFSDK
+//
+//  Created on 2017/03/23.
+//  Copyright (c) 2017 Kii. All rights reserved.
+//
+
+import XCTest
+
+@testable import ThingIFSDK
+
+class ThingIFAPIPostNewTriggerTests: SmallTestBase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testPostNewTrigger_success() throws {
+        let setting = TestSetting()
+        let api = setting.api
+        let target = setting.target
+        // perform onboarding
+        api.target = target
+
+        let equalColor0 =
+          EqualsClauseInTrigger("alias1", field: "color", intValue: 0)
+        let equalPowerTrue =
+          EqualsClauseInTrigger("alias1", field: "power", boolValue: true)
+        let equalBrightness50 =
+          EqualsClauseInTrigger("alias1", field: "brightness", intValue: 50)
+        let lessThanOrEqualColor255 = RangeClauseInTrigger.lessThanOrEqualTo(
+          "alias1",
+          field: "color",
+          limit: 255)
+        let lessThanColor200 =
+          RangeClauseInTrigger.lessThan("alias1", field: "color", limit: 200)
+        let lessThanColor200_345 =
+          RangeClauseInTrigger.lessThan(
+            "alias1",
+            field: "color",
+            limit: 200.345)
+        let greaterThanOrEqualColor1 =
+          RangeClauseInTrigger.greaterThanOrEqualTo(
+            "alias1",
+            field: "color",
+            limit: 1)
+        let greaterThanColor1 = RangeClauseInTrigger.greaterThan(
+          "alias1",
+          field: "color",
+          limit: 1)
+        let greaterThanColor1_345 = RangeClauseInTrigger.greaterThan(
+          "alias1",
+          field: "color",
+          limit: 1.345)
+        let rangeInclude1ToInclude345 =
+          RangeClauseInTrigger.range(
+            "alias1",
+            field: "color",
+            lowerLimit: 1,
+            lowerIncluded: true,
+            upperLimit: 345,
+            upperIncluded: true)
+        let rangeInclude1_1ToInclude345_3 =
+          RangeClauseInTrigger.range(
+            "alias1",
+            field: "color",
+            lowerLimit: 1.1,
+            lowerIncluded: true,
+            upperLimit: 345.3,
+            upperIncluded: true)
+
+        let predicates: [Predicate & ToJsonObject] = [
+          // Schedule once
+          ScheduleOncePredicate(Date(timeIntervalSinceNow: 10)),
+          // Schedule
+          SchedulePredicate("00 * * * *"),
+          // state predicate
+          StatePredicate(
+            Condition(equalColor0),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(equalPowerTrue),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(NotEqualsClauseInTrigger(equalPowerTrue)),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(lessThanOrEqualColor255),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(lessThanColor200),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(lessThanColor200_345),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(greaterThanOrEqualColor1),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(greaterThanColor1),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(greaterThanColor1_345),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(rangeInclude1ToInclude345),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(rangeInclude1_1ToInclude345_3),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(
+              AndClauseInTrigger(
+                equalColor0,
+                NotEqualsClauseInTrigger(equalPowerTrue))),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(
+              OrClauseInTrigger(
+                equalColor0,
+                NotEqualsClauseInTrigger(equalPowerTrue))),
+            triggersWhen: .conditionFalseToTrue),
+          // complex clauses
+          StatePredicate(
+            Condition(
+              AndClauseInTrigger(
+                equalBrightness50,
+                OrClauseInTrigger(
+                  equalColor0,
+                  NotEqualsClauseInTrigger(equalPowerTrue)))),
+            triggersWhen: .conditionFalseToTrue),
+          StatePredicate(
+            Condition(
+              OrClauseInTrigger(
+                equalBrightness50,
+                AndClauseInTrigger(
+                  equalColor0,
+                  NotEqualsClauseInTrigger(equalPowerTrue)))),
+            triggersWhen: .conditionFalseToTrue),
+          // test triggersWhen
+          StatePredicate(
+            Condition(equalColor0),
+            triggersWhen: .conditionChanged),
+          StatePredicate(
+            Condition(equalColor0),
+            triggersWhen: .conditionTrue)
+        ]
+
+        for (index, predicate) in predicates.enumerated() {
+            try postNewTriggerSuccess(
+              "testPostNewTrigger_success_\(index)",
+              predicate: predicate,
+              setting: setting)
+        }
+
+    }
+
+    func postNewTriggerSuccess(
+      _ tag: String,
+      predicate: Predicate & ToJsonObject,
+      setting:TestSetting) throws -> Void
+    {
+        let expectation = self.expectation(description: tag)
+
+        let expectedTriggerID = "0267251d9d60-1858-5e11-3dc3-00f3f0b5"
+
+        iotSession = MockMultipleSession.self
+        let expectedAliasActions = [
+          AliasAction(
+            "alias1",
+            actions: [
+              Action("turnPower", value: true),
+              Action("setBrightness", value: 90)
+            ]
+          )
+        ]
+        let expectedTriggerdCommandForm = TriggeredCommandForm(
+          expectedAliasActions
+        )
+        let expectedTrigger = Trigger(
+          expectedTriggerID,
+          targetID: setting.target.typedID,
+          enabled: true,
+          predicate: predicate,
+          command: Command(
+            "dummyCommandID",
+            targetID: setting.target.typedID,
+            issuerID: setting.owner.typedID,
+            aliasActions: expectedAliasActions,
+            commandState: .sending,
+            created: Date()
+          )
+        )
+
+        sharedMockMultipleSession.responsePairs = [
+          (
+            (
+              try JSONSerialization.data(
+                withJSONObject:["triggerID": expectedTriggerID],
+                options: .prettyPrinted),
+              HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 201,
+                httpVersion: nil,
+                headerFields: nil),
+              nil
+            ),
+            makeRequestVerifier() { request in
+                XCTAssertEqual(request.httpMethod, "POST")
+
+                XCTAssertEqual(
+                  setting.app.baseURL + "/thing-if/apps/\(setting.app.appID)/targets/\(setting.target.typedID.toString())/triggers",
+                  request.url?.absoluteString)
+
+                //verify header
+                XCTAssertEqual(
+                  [
+                    "X-Kii-AppID": setting.app.appID,
+                    "X-Kii-AppKey": setting.app.appKey,
+                    "X-Kii-SDK" : SDKVersion.sharedInstance.kiiSDKHeader,
+                    "Authorization": "Bearer \(setting.owner.accessToken)",
+                    "Content-Type": "application/json"
+                  ],
+                  request.allHTTPHeaderFields!)
+
+                //verify body
+                var commandJson = expectedTriggerdCommandForm.makeJsonObject()
+                commandJson["issuer"] = setting.owner.typedID.toString()
+                if commandJson["target"] == nil {
+                    commandJson["target"] = setting.target.typedID.toString()
+                }
+                XCTAssertEqual(
+                  [
+                    "predicate" : predicate.makeJsonObject(),
+                    "command" : commandJson,
+                    "triggersWhat" : TriggersWhat.command.rawValue
+                  ] as NSDictionary,
+                  try JSONSerialization.jsonObject(
+                    with: request.httpBody!,
+                    options: JSONSerialization.ReadingOptions.allowFragments)
+                    as? NSDictionary
+                )
+            }
+          ),
+          (
+            (
+              try JSONSerialization.data(
+                withJSONObject: expectedTrigger.makeJsonObject(),
+                options: .prettyPrinted),
+              HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 200,
+                httpVersion: nil,
+                headerFields: nil),
+              nil
+            ),
+            { request in
+                XCTAssertEqual(request.httpMethod, "GET", tag)
+                //verify header
+                XCTAssertEqual(
+                  setting.app.baseURL + "/thing-if/apps/50a62843/targets/\(setting.target.typedID.toString())/triggers/\(expectedTriggerID)",
+                  request.url?.absoluteString)
+
+                //verify header
+                XCTAssertEqual(
+                  [
+                    "X-Kii-AppID": setting.app.appID,
+                    "X-Kii-AppKey": setting.app.appKey,
+                    "X-Kii-SDK" : SDKVersion.sharedInstance.kiiSDKHeader,
+                    "Authorization": "Bearer \(setting.owner.accessToken)"
+                  ],
+                  request.allHTTPHeaderFields!,
+                  tag)
+            }
+          )
+        ]
+
+        setting.api.postNewTrigger(
+          expectedTriggerdCommandForm,
+          predicate: predicate) { trigger, error -> Void in
+            XCTAssertNil(error)
+            XCTAssertEqual(expectedTrigger, trigger)
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error, tag)
+        }
+    }
+
+}
