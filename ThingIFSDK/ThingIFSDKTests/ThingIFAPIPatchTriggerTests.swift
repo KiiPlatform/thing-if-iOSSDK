be834c52edcf96b522bd4564b89e802056e8474b
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-03-27 16:50:45 +0900

    Add a test.

---
+++ ThingIFSDK/ThingIFSDKTests/ThingIFAPIPatchTriggerTests.swift
@@ -0,0 +1,371 @@
+//
+//  ThingIFAPIPatchTriggerTests.swift
+//  ThingIFSDK
+//
+//  Created on 2017/03/27.
+//  Copyright (c) 2017 Kii. All rights reserved.
+//
+
+import XCTest
+
+@testable import ThingIFSDK
+
+class ThingIFAPIPatchTriggerTests: SmallTestBase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    static let DEFAULT_COMMAND = Command(
+      "429251a0-46f7-11e5-a5eb-06d9d1527620",
+      targetID: TestSetting().target.typedID,
+      issuerID: TestSetting().owner.typedID,
+      aliasActions: [
+        AliasAction(
+          "alias1",
+          actions: [
+            Action("turnPower", value: true),
+            Action("setBrightness", value: 90)
+          ]
+        )
+      ],
+      commandState: .sending,
+      created: Date()
+    )
+
+    static let defaultTrigger = Trigger(
+      "0267251d9d60-1858-5e11-3dc3-00f3f0b5",
+      targetID: TestSetting().target.typedID,
+      enabled: true,
+      predicate: StatePredicate(
+        Condition(
+          EqualsClauseInTrigger(
+            "alias1",
+            field: "color",
+            intValue: 0)),
+        triggersWhen: .conditionFalseToTrue),
+      command: DEFAULT_COMMAND
+    )
+
+    struct TestCase {
+        let input: (TriggeredCommandForm?, Predicate?, TriggerOptions?)?
+        let output: Trigger
+
+        init(
+          _ input: (TriggeredCommandForm?, Predicate?, TriggerOptions?)? =
+            (nil, nil, TriggerOptions("title")),
+          output: Trigger = defaultTrigger
+        )
+        {
+            self.input = input
+            self.output = output
+        }
+    }
+
+    func testPatchTrigger() throws {
+
+        let setting = TestSetting()
+
+        let command = ThingIFAPIPatchTriggerTests.DEFAULT_COMMAND
+
+
+        let testsCases: [TestCase] = [
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ]
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ],
+                targetID: TypedID(.thing, id: "dummyCommandID")
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ],
+                title: "title"
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ],
+                commandDescription: "description"
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ],
+                metadata: ["key" : "value"]
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            (
+              TriggeredCommandForm(
+                [
+                  AliasAction(
+                    "alias1",
+                    actions: [
+                      Action("turnPower", value: true),
+                      Action("setBrightness", value: 90)
+                    ]
+                  )
+                ],
+                targetID: TypedID(.thing, id: "dummyCommandID"),
+                title: "title",
+                commandDescription: "description",
+                metadata: ["key" : "value"]
+              ),
+              nil,
+              nil
+            )
+          ),
+          TestCase(
+            output: Trigger(
+              "0267251d9d60-1858-5e11-3dc3-00f3f0b5",
+              targetID: setting.target.typedID,
+              enabled: true,
+              predicate: StatePredicate(
+                Condition(
+                  EqualsClauseInTrigger(
+                    "alias1",
+                    field: "color",
+                    intValue: 0)),
+                triggersWhen: .conditionFalseToTrue),
+              command: command)),
+          TestCase(
+            output: Trigger(
+              "0267251d9d60-1858-5e11-3dc3-00f3f0b5",
+              targetID: setting.target.typedID,
+              enabled: true,
+              predicate: StatePredicate(
+                Condition(
+                  NotEqualsClauseInTrigger(
+                    EqualsClauseInTrigger(
+                      "alias1",
+                      field: "power",
+                      boolValue: true))),
+                triggersWhen: .conditionFalseToTrue),
+              command: command)),
+          TestCase(
+            output: Trigger(
+              "0267251d9d60-1858-5e11-3dc3-00f3f0b5",
+              targetID: setting.target.typedID,
+              enabled: true,
+              predicate: SchedulePredicate("00 * * * *"),
+              command: command)),
+          TestCase(
+            output: Trigger(
+              "0267251d9d60-1858-5e11-3dc3-00f3f0b5",
+              targetID: setting.target.typedID,
+              enabled: true,
+              predicate: ScheduleOncePredicate(
+                Date(timeIntervalSinceNow: 1000)),
+              command: command))
+        ]
+
+        for (index,testCase) in testsCases.enumerated() {
+            try patchTrigger(
+              "testPatchTrigger_\(index)",
+              testcase: testCase,
+              setting: setting)
+        }
+    }
+
+    func patchTrigger(
+      _ tag: String,
+      testcase: TestCase,
+      setting: TestSetting) throws -> Void
+    {
+        let expectation : XCTestExpectation! =
+          self.expectation(description: tag)
+
+        iotSession = MockMultipleSession.self
+        sharedMockMultipleSession.responsePairs = [
+          // Patch request and response
+          (
+            (
+              nil,
+              HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 204,
+                httpVersion: nil,
+                headerFields: nil),
+              nil
+            ),
+            makeRequestVerifier() { request in
+                XCTAssertEqual(request.httpMethod, "PATCH")
+                XCTAssertEqual(
+                  setting.app.baseURL + "/thing-if/apps/50a62843/targets/\(setting.target.typedID.toString())/triggers/\(testcase.output.triggerID)",
+                  request.url?.absoluteString,
+                  tag)
+                //verify header
+                XCTAssertEqual(
+                  [
+                    "X-Kii-AppID": setting.app.appID,
+                    "X-Kii-AppKey": setting.app.appKey,
+                    "X-Kii-SDK" : SDKVersion.sharedInstance.kiiSDKHeader,
+                    "Authorization": "Bearer \(setting.owner.accessToken)",
+                    "Content-Type": "application/json"
+                  ],
+                  request.allHTTPHeaderFields!,
+                  tag)
+                //verify body
+                if let requestBody = self.makePatchRequestBody(
+                     testcase.input,
+                     setting: setting) {
+                    XCTAssertEqual(
+                      requestBody as NSDictionary,
+                      try JSONSerialization.jsonObject(
+                        with: request.httpBody!,
+                        options: .mutableContainers) as? NSDictionary,
+                      tag)
+                } else {
+                    XCTAssertNil(request.httpBody)
+                }
+            }
+          ),
+          // Get reqeust and response
+          (
+            (
+              try JSONSerialization.data(
+                withJSONObject: testcase.output.makeJsonObject(),
+                options: .prettyPrinted),
+              HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 201,
+                httpVersion: nil,
+                headerFields: nil),
+              nil
+            ),
+            makeRequestVerifier() { request in
+                XCTAssertEqual(request.httpMethod, "GET", tag)
+                //verify header
+                XCTAssertEqual(
+                  setting.app.baseURL + "/thing-if/apps/50a62843/targets/\(setting.target.typedID.toString())/triggers/\(testcase.output.triggerID)",
+                  request.url?.absoluteString)
+
+                //verify header
+                XCTAssertEqual(
+                  [
+                    "X-Kii-AppID": setting.app.appID,
+                    "X-Kii-AppKey": setting.app.appKey,
+                    "X-Kii-SDK" : SDKVersion.sharedInstance.kiiSDKHeader,
+                    "Authorization": "Bearer \(setting.owner.accessToken)"
+                  ],
+                  request.allHTTPHeaderFields!,
+                  tag)
+            }
+          )
+        ]
+
+        // perform onboarding.
+        let api = setting.api
+        api.target = setting.target
+
+        api.patchTrigger(
+          testcase.output.triggerID,
+          triggeredCommandForm: testcase.input?.0,
+          predicate: testcase.input?.1,
+          options: testcase.input?.2) { trigger, error -> Void in
+
+            XCTAssertNil(error)
+            XCTAssertEqual(testcase.output, trigger)
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error, tag)
+        }
+    }
+
+    private func makePatchRequestBody(
+      _ input: (
+        form: TriggeredCommandForm?,
+        predicate: Predicate?,
+        options: TriggerOptions?)?,
+      setting: TestSetting) -> [String : Any]?
+    {
+        guard let input = input else {
+            return nil
+        }
+
+        var retval = input.options?.makeJsonObject() ?? [ : ]
+        retval["predicate"] =
+          (input.predicate as? ToJsonObject)?.makeJsonObject()
+        if let form = input.form {
+            var commandJson = form.makeJsonObject()
+            commandJson["issuer"] = setting.owner.typedID.toString()
+            if commandJson["target"] == nil {
+                commandJson["target"] = setting.target.typedID.toString()
+            }
+            retval["command"] = commandJson
+        }
+
+        return retval.isEmpty ? nil : retval
+    }
+}
