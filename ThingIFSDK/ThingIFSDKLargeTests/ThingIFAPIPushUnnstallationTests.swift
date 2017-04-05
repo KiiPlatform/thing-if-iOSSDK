efabeba0c88984487c01ad369dbe5affd91b7551
rizakii <riza.alaudin@kii.com>
2017-04-05 16:17:04 +0900

    add testcases.

--- ThingIFSDK/ThingIFSDKLargeTests/ThingIFAPIPushUnnstallationTests.swift
+++ ThingIFSDK/ThingIFSDKLargeTests/ThingIFAPIPushUnnstallationTests.swift
@@ -18,7 +18,61 @@ class ThingIFAPIPushUninstallationTests: OnboardedTestsBase {
         super.tearDown()
     }
 
+    func testUninstallPushSuccess()  {
+        let dummyDevice = NSUUID().uuidString.data(using: .ascii)!
 
+        let expectation = self.expectation(description: "install before uninstall")
+
+        onboardedApi.installPush(dummyDevice) { (installationID, error) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(installationID)
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+
+        let expectation2 = self.expectation(description: "testUninstallPushSuccess")
+        onboardedApi.uninstallPush { (error) in
+            XCTAssertNil(error)
+            expectation2.fulfill()
+        }
+
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+
+    }
+
+    func testUninstallPushWithInstallationIDSuccess()  {
+        let dummyDevice = NSUUID().uuidString.data(using: .ascii)!
+
+        let expectation = self.expectation(description: "install before uninstall")
+        var installationID : String?
+
+        onboardedApi.installPush(dummyDevice) { (retInstallationID, error) in
+            XCTAssertNil(error)
+            XCTAssertNotNil(retInstallationID)
+            installationID = retInstallationID
+            expectation.fulfill()
+        }
+
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+
+        let expectation2 = self.expectation(description: "testUninstallPushSuccess")
+        onboardedApi.uninstallPush(installationID) { (error) in
+            XCTAssertNil(error)
+            expectation2.fulfill()
+        }
+
+        self.waitForExpectations(timeout: TEST_TIMEOUT) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+        
+    }
 
     func testUninstallPushError() {
 
