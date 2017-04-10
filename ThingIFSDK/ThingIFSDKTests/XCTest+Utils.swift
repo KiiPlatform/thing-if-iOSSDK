c4541a8073ebd759334025b9f7cfa951a1002048
Shinsuke Nara <shinsuke.nara@muraoka-design.com>
2017-04-10 15:33:15 +0900

    Remove unused test utility methods.

--- ThingIFSDK/ThingIFSDKTests/XCTest+Utils.swift
+++ ThingIFSDK/ThingIFSDKTests/XCTest+Utils.swift
@@ -159,40 +159,7 @@ extension XCTestCase {
             return
         }
     }
-
-    func verifyArray(_ expected: [Any]?,
-                     actual: [Any]?,
-                     message: String? = nil)
-    {
-        if expected == nil && actual == nil {
-            return
-        }
-        guard let expected2 = expected, let actual2 = actual else {
-            XCTFail("one of expected or actual is nil")
-            return
-        }
-
-        let error_message: String
-        if let message2 = message {
-            error_message = message2 + ", expected=" + expected2.description +
-              "actual=" + actual2.description
-        } else {
-            error_message = "expected=" + expected2.description +
-              "actual=" + actual2.description
-        }
-        XCTAssertEqual(NSArray(array: expected2), NSArray(array: actual2),
-                      error_message)
-    }
     
-    func verifyDict2(_ expectedDict:Dictionary<String, Any>?, _ actualDict: Dictionary<String, Any>?, _ errorMessage: String? = nil){
-        if expectedDict == nil && actualDict == nil {
-            return
-        }
-        verifyDict(expectedDict!,
-                   actualDict: actualDict,
-                   errorMessage: errorMessage)
-    }
-
     func verifyDict(_ expectedDict:Dictionary<String, Any>, actualDict: Dictionary<String, Any>?, errorMessage: String? = nil){
         guard let actualDict2 = actualDict else {
             XCTFail("actualDict must not be nil")
@@ -209,10 +176,6 @@ extension XCTestCase {
         }
         XCTAssertTrue(NSDictionary(dictionary: expectedDict).isEqual(to: actualDict2), s)
     }
-    func verifyNsDict(_ expectedDict:NSDictionary, actualDict:NSDictionary){
-        let s = "expected=" + expectedDict.description + "actual" + actualDict.description
-        XCTAssertTrue(expectedDict.isEqual(to: actualDict as! [AnyHashable: Any]), s)
-    }
     
     func verifyDict(_ expectedDict:Dictionary<String, Any>, actualData: Data){
         
@@ -225,14 +188,4 @@ extension XCTestCase {
         }
     }
 
-    func XCTAssertEqualDictionaries<S, T: Equatable>(_ first: [S:T], _ second: [S:T]) {
-        XCTAssert(first == second)
-    }
-
-    func XCTAssertEqualIoTAPIWithoutTarget(_ first: ThingIFAPI, _ second: ThingIFAPI) {
-        XCTAssertEqual(first.appID, second.appID)
-        XCTAssertEqual(first.appKey, second.appKey)
-        XCTAssertEqual(first.baseURL, second.baseURL)
-        XCTAssertEqual(first.installationID, second.installationID)
-    }
 }
