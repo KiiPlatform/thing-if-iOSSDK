76e8bcc4f2e7cfbb81fc563d793488ce35ad4b1c
rizakii <riza.alaudin@kii.com>
2017-04-17 15:17:13 +0900

    Add login failed test case.

--- ThingIFSDK/ThingIFSDKTests/GatewayAPILoadInstanceErrorTests.swift
+++ ThingIFSDK/ThingIFSDKTests/GatewayAPILoadInstanceErrorTests.swift
@@ -10,10 +10,15 @@ import XCTest
 @testable import ThingIFSDK
 
 class GatewayAPILoadInstanceErrorTests: SmallTestBase {
-
-    func testLoadStoredInstanceError(){
+    override func setUp() {
+        super.setUp()
         //removing all gateway stored instance
         GatewayAPI.removeAllStoredInstances()
+
+    }
+
+    func testLoadStoredInstanceError(){
+
         //load stored instance should throw error
         XCTAssertThrowsError(try GatewayAPI.loadWithStoredInstance()) { error in
             XCTAssertEqual(
@@ -21,4 +26,72 @@ class GatewayAPILoadInstanceErrorTests: SmallTestBase {
                 error as? ThingIFError)
         }
     }
+
+    func testLogin400Error() throws {
+
+        let expectation = self.expectation(description: "testLogin400Error")
+        let setting = TestSetting()
+
+        // mock response
+        sharedMockSession.mockResponse = (
+            nil,
+            HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 400,
+                httpVersion: nil,
+                headerFields: nil),
+            nil)
+        iotSession = MockSession.self
+
+        let gatewayAPI = GatewayAPI(
+            setting.app,
+            gatewayAddress: URL(string: setting.app.baseURL)!)
+        gatewayAPI.login("dummyUser", password: "dummyPass") {_ in expectation.fulfill() }
+
+        self.waitForExpectations(timeout: 20.0) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+
+        //load stored instance should throw error
+        XCTAssertThrowsError(try GatewayAPI.loadWithStoredInstance()) { error in
+            XCTAssertEqual(
+                ThingIFError.apiNotStored(tag: nil),
+                error as? ThingIFError)
+        }
+
+    }
+
+    func testLogin401Error() throws {
+        let expectation = self.expectation(description: "testLogin401Error")
+        let setting = TestSetting()
+
+        // mock response
+        sharedMockSession.mockResponse = (
+            nil,
+            HTTPURLResponse(
+                url: URL(string:setting.app.baseURL)!,
+                statusCode: 401,
+                httpVersion: nil,
+                headerFields: nil),
+            nil)
+        iotSession = MockSession.self
+
+        let gatewayAPI = GatewayAPI(
+            setting.app,
+            gatewayAddress: URL(string: setting.app.baseURL)!)
+        gatewayAPI.login("dummyUser", password: "dummyPass") {_ in expectation.fulfill() }
+        
+        self.waitForExpectations(timeout: 20.0) { (error) -> Void in
+            XCTAssertNil(error)
+        }
+
+        //load stored instance should throw error
+        XCTAssertThrowsError(try GatewayAPI.loadWithStoredInstance()) { error in
+            XCTAssertEqual(
+                ThingIFError.apiNotStored(tag: nil),
+                error as? ThingIFError)
+        }
+        
+    }
+
 }
